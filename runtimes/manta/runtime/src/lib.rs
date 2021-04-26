2c6e8f815b47a11611f95a041ba2c516aede2b4a
zhenfei <zhenfei.zhang@hotmail.com>
2021-04-26 16:41:14 -0400

    update ss58prefix to

--- runtimes/manta/runtime/src/lib.rs
+++ runtimes/manta/runtime/src/lib.rs
@@ -148,7 +148,7 @@ parameter_types! {
         ::with_sensible_defaults(2 * WEIGHT_PER_SECOND, NORMAL_DISPATCH_RATIO);
     pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
         ::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
-    pub const SS58Prefix: u8 = 42;
+    pub const SS58Prefix: u8 = 77;
 }
 
 // Configure FRAME pallets to include in runtime.
