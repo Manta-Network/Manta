67261e68a991b69e614ef317f888d9b5919a25dc
stechu <shumo.chu@gmail.com>
2021-05-14 18:00:10 -0700

    benchmark works

--- runtimes/manta/runtime/src/lib.rs
+++ runtimes/manta/runtime/src/lib.rs
@@ -731,6 +731,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
 			add_benchmark!(params, batches, pallet_balances, Balances);
 			add_benchmark!(params, batches, pallet_timestamp, Timestamp);
+			add_benchmark!(params, batches, pallet_manta_pay, MantaPay);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
