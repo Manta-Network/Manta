5da4bea6b733828738af4862c840237a63a04e24
Dengjianping <djptux@gmail.com>
2021-06-02 23:44:09 +0800

    Add pallet-collator-selection, align manta-pc'deps with the statemint's

--- node/src/service.rs
+++ node/src/service.rs
@@ -359,7 +359,7 @@ where
 	let transaction_pool = params.transaction_pool.clone();
 	let mut task_manager = params.task_manager;
 	let import_queue = cumulus_client_service::SharedImportQueue::new(params.import_queue);
-	let (network, network_status_sinks, system_rpc_tx, start_network) =
+	let (network, system_rpc_tx, start_network) =
 		sc_service::build_network(sc_service::BuildNetworkParams {
 			config: &parachain_config,
 			client: client.clone(),
@@ -384,7 +384,6 @@ where
 		keystore: params.keystore_container.sync_keystore(),
 		backend: backend.clone(),
 		network: network.clone(),
-		network_status_sinks,
 		system_rpc_tx,
 		telemetry: telemetry.as_mut(),
 	})?;
