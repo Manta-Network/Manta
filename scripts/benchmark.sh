4ddb3972285a74ca78a48b42e24cce48ccd52f68
Georgi Zlatarev <45011053+ghzlatarev@users.noreply.github.com>
2021-07-16 23:21:59 +0300

    [Manta] Sync to latest commits for manta dependencies (#92)
    * Sync commit for manta-crypto update
    
    * Update manta-dependencies to latest commits

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-cargo +nightly build --features runtime-benchmarks --release
+cargo build --features runtime-benchmarks --release
 
 target/release/manta benchmark --pallet pallet_manta_pay --extrinsic init_asset --execution=Wasm --wasm-execution Compiled --repeat 100 --log warn
 target/release/manta benchmark --pallet pallet_manta_pay --extrinsic transfer_asset --execution=Wasm --wasm-execution Compiled --repeat 100 --log warn
