c5fcf3fc02a929260b14d7a8745714e96b446415
ghzlatarev <ghzlatarev@gmail.com>
2021-09-20 22:11:50 +0300

    Revert chain spec version to 1

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -82,7 +82,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("calamari"),
 	impl_name: create_runtime_str!("calamari"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 1,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
