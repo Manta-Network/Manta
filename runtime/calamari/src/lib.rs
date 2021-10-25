9fa27dae078b95646b362150c843fa4d1b87c829
ghzlatarev <ghzlatarev@gmail.com>
2021-10-25 13:38:55 +0300

    Bump spec_version to 4 after hotfix upgrade

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("calamari"),
 	impl_name: create_runtime_str!("calamari"),
 	authoring_version: 1,
-	spec_version: 3,
+	spec_version: 4,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
