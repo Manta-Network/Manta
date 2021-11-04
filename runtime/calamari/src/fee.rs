0aec9478ce625f1127633da5ca4e1b6f5d0f2c8d
stechu <shumo.chu@protonmail.com>
2021-11-03 19:00:17 -0700

    add headers

--- runtime/calamari/src/fee.rs
+++ runtime/calamari/src/fee.rs
@@ -1,3 +1,19 @@
+// Copyright 2019-2021 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::currency;
 use frame_support::weights::{
 	constants::ExtrinsicBaseWeight, WeightToFeeCoefficient, WeightToFeeCoefficients,
--- runtime/manta/src/currency.rs
+++ runtime/manta/src/currency.rs
@@ -1,4 +1,20 @@
-#![allow(non_camel_case_types)]
+// Copyright 2019-2021 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+
+#![allow(non_upper_case_globals)]
 use manta_primitives::Balance;
 
 pub const MA: Balance = 1_000_000_000_000_000_000; // 18 decimal
