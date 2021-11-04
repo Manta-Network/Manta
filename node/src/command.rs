2a8526df680dc20459866aa62005d15ad1200a79
stechu <shumo.chu@protonmail.com>
2021-11-04 03:47:31 -0700

    remove try_runtime from command

--- node/src/command.rs
+++ node/src/command.rs
@@ -324,30 +324,6 @@ pub fn run() -> Result<()> {
 					.into())
 			}
 		}
-		#[cfg(feature = "try-runtime")]
-		Some(Subcommand::TryRuntime(cmd)) => {
-			let runner = cli.create_runner(cmd)?;
-			runner.async_run(|config| {
-				// we don't need any of the components of new_partial, just a runtime, or a task
-				// manager to do `async_run`.
-				let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
-				let task_manager =
-					sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
-						.map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
-
-				if runner.config().chain_spec.is_manta() {
-					Ok((cmd.run::<Block, MantaRuntimeExecutor>(config), task_manager))
-				} else if runner.config().chain_spec.is_calamari() {
-					Ok((
-						cmd.run::<Block, CalamariRuntimeExecutor>(config),
-						task_manager,
-					))
-				} else {
-					Err("chain spec doesn't support try_runtime.").into()
-				}
-			})
-		}
-		#[cfg(not(feature = "try-runtime"))]
 		Some(Subcommand::TryRuntime) => Err("Try-runtime wasn't enabled when building the node. \
 		You can enable it with `--features try-runtime`."
 			.into()),
