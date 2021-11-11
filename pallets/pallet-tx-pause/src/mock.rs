32b863b820f81f95d3f0b3281411971abbe0fd73
ghzlatarev <ghzlatarev@gmail.com>
2021-11-11 22:11:50 +0200

    Add missing pallets/ folder

---
+++ pallets/pallet-tx-pause/src/mock.rs
@@ -0,0 +1,124 @@
+// Copyright 2020-2021 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Mocks for the tx pause pallet.
+
+#![cfg(test)]
+
+use super::*;
+use frame_support::{construct_runtime, ord_parameter_types, parameter_types, traits::Everything};
+use frame_system::EnsureSignedBy;
+use manta_primitives::Balance;
+
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::IdentityLookup};
+
+pub type AccountId = u128;
+pub const ALICE: AccountId = 1;
+
+mod tx_pause {
+	pub use super::super::*;
+}
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+}
+
+impl frame_system::Config for Runtime {
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Call = Call;
+	type Hash = H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type DbWeight = ();
+	type BaseCallFilter = Everything;
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+}
+
+parameter_types! {
+	pub const NativeTokenExistentialDeposit: Balance = 10;
+	pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Runtime {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = NativeTokenExistentialDeposit;
+	type AccountStore = System;
+	type MaxLocks = ();
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = ();
+	type WeightInfo = ();
+}
+
+ord_parameter_types! {
+	pub const One: AccountId = 1;
+}
+
+impl Config for Runtime {
+	type Event = Event;
+	type UpdateOrigin = EnsureSignedBy<One, AccountId>;
+	type WeightInfo = ();
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		TransactionPause: tx_pause::{Pallet, Storage, Call, Event<T>},
+		Balances: pallet_balances::{Pallet, Storage, Call, Event<T>},
+	}
+);
+
+pub struct ExtBuilder;
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		ExtBuilder
+	}
+}
+
+impl ExtBuilder {
+	pub fn build(self) -> sp_io::TestExternalities {
+		let t = frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.unwrap();
+
+		t.into()
+	}
+}
