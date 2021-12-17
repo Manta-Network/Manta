b3aef500ac6070d6ac92ab726227ee5554236e16
Georgi Zlatarev <45011053+ghzlatarev@users.noreply.github.com>
2021-12-17 16:26:58 +0200

    [Manta] CI runtime upgrade test for manta parachain (#283)

--- node/src/command.rs
+++ node/src/command.rs
@@ -72,6 +72,7 @@ fn load_spec(
 		"manta-dev" => Ok(Box::new(chain_specs::manta_development_config(para_id))),
 		"manta-local" => Ok(Box::new(chain_specs::manta_local_config(para_id))),
 		"manta-testnet" => Ok(Box::new(chain_specs::manta_testnet_config(para_id)?)),
+		"manta-testnet-ci" => Ok(Box::new(chain_specs::manta_testnet_ci_config(para_id)?)),
 		"manta" => Ok(Box::new(chain_specs::manta_config()?)),
 		// calamari chainspec
 		"calamari-dev" => Ok(Box::new(chain_specs::calamari_development_config(para_id))),
