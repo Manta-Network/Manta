b92f5eec3b6f9ee33074626d24224436510b43c1
Dengjianping <djptux@gmail.com>
2021-12-30 10:48:43 +0800

    Rollback spec-version

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -110,7 +110,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("manta"),
 	impl_name: create_runtime_str!("manta"),
 	authoring_version: 1,
-	spec_version: 3120,
+	spec_version: 3110,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
