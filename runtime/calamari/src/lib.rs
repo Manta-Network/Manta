9116f443b0e4ab4b501d78b6a1c41da1a3f74e6e
ghzlatarev <ghzlatarev@gmail.com>
2022-01-18 20:18:46 +0200

    Bump spec version to apply the tips patch to our testnets

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("calamari"),
 	impl_name: create_runtime_str!("calamari"),
 	authoring_version: 1,
-	spec_version: 3120,
+	spec_version: 3121,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
