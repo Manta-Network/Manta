b107e44bb8050f4230dc2ed7f0a108c06952122e
Dengjianping <djptux@gmail.com>
2022-01-20 18:11:37 +0800

    Bump deps to polkadot-v0.9.15

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -915,7 +915,8 @@ pub type Executive = frame_executive::Executive<
 	Block,
 	frame_system::ChainContext<Runtime>,
 	Runtime,
-	AllPallets,
+	AllPalletsWithSystem,
+	()
 >;
 
 impl_runtime_apis! {
--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -643,7 +643,8 @@ pub type Executive = frame_executive::Executive<
 	Block,
 	frame_system::ChainContext<Runtime>,
 	Runtime,
-	AllPallets,
+	AllPalletsWithSystem,
+	()
 >;
 
 impl_runtime_apis! {
