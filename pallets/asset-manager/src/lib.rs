3275af9539494fac0ff55cb335d9e4379985cdaf
Shumo Chu <shumo.chu@protonmail.com>
2022-01-22 11:33:56 -0500

    wip

--- pallets/asset-manager/src/lib.rs
+++ pallets/asset-manager/src/lib.rs
@@ -26,9 +26,9 @@
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{pallet_prelude::*, PalletId};
+	use frame_support::{pallet_prelude::*, transactional, PalletId};
 	use frame_system::pallet_prelude::*;
-	use codec::HasCompact;
+	use codec::{HasCompact, Codec};
 	use scale_info::TypeInfo;
 	use sp_runtime::{traits::{AtLeast32BitUnsigned, CheckedAdd, Bounded, One}, ArithmeticError};
 
@@ -37,6 +37,33 @@ pub mod pallet {
 
 	/// The AssetManagers's pallet id
 	pub const PALLET_ID: PalletId = PalletId(*b"asstmngr");
+
+	/// The registrar trait: defines the interface of creating an asset in the asset implementation layer.
+	/// We may revisit this interface design (e.g. add change asset interface). However, change StorageMetadata 
+	/// should be rare.
+	pub trait AssetRegistrar<T: Config> {
+		///
+		/// * `asset_id`: the asset id to be created
+		/// * `min_balance`: the minimum balance to hold this asset
+		/// * `metadata`: the metadata that the implementation layer stores
+		/// * `is_sufficient`: Whether this asset needs users to have an existential deposit to hold
+	    ///  this asset.
+		fn create_asset(
+			asset_id: T::AssetId,
+			min_balance: T::Balance,
+			metadata: T::StorageMetadata,
+			is_sufficient: bool,
+		) -> DispatchResult;
+	}
+
+	/// The AssetMetadata trait:
+	pub trait AssetMetadata<T: Config> {
+		/// Returns the minimum balance to hold this asset
+		fn min_balance(&self) -> T::Balance;
+
+		/// Returns a boolean value indicating whether this asset needs an existential deposit
+		fn is_sufficient(&self) -> bool;
+	}
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -57,6 +84,9 @@ pub mod pallet {
 			+ MaxEncodedLen
 			+ TypeInfo;
 		
+		/// The trait we use to register Assets
+		type AssetRegistrar: AssetRegistrar<Self>;
+		
 		/// The units in which we record balances.
 		type Balance: Member + Parameter + AtLeast32BitUnsigned + Default + Copy + MaxEncodedLen;
 		
@@ -64,7 +94,7 @@ pub mod pallet {
 		type StorageMetadata: Member + Parameter + Default;
 
 		/// The Asset Metadata type stored in this pallet.
-		type AssetRegistrarMetadata: Member + Parameter + Default + Into<Self::StorageMetadata>;
+		type AssetRegistrarMetadata: Member + Parameter + Codec + Default + Into<Self::StorageMetadata> + AssetMetadata<Self>;
 
 		/// The AssetLocation type: could be just a thin wrapper of MultiLocation
 		type AssetLocation: Member + Parameter + Default;
@@ -103,15 +133,15 @@ pub mod pallet {
 		}
 	}
 
-	/// Error for the nicks pallet.
+	/// Error.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// A name is too short.
-		TooShort,
-		/// A name is too long.
-		TooLong,
-		/// An account isn't named.
-		Unnamed,
+		/// Location already exists.
+		LocationAlreadyExists,
+		/// Error creating asset, e.g. error returned from the implementation layer.
+		ErrorCreatingAsset,
+		/// Update a non-exist asset
+		UpdateNonExistAsset,
 	}
 
 	/// AssetId to MultiLocation Map.
@@ -134,15 +164,26 @@ pub mod pallet {
 	pub(super) type AssetIdMetadata<T: Config> =
 		StorageMap<_, Blake2_128Concat, T::AssetId, T::AssetRegistrarMetadata>;
 	
-	/// Get the next available AssetId
+	/// Get the next available AssetId.
 	#[pallet::storage]
 	#[pallet::getter(fn next_asset_id)]
 	pub type NextAssetId<T: Config> = StorageValue<_, T::AssetId, ValueQuery>;
+
+	/// XCM transfer cost for different asset.
+	#[pallet::storage]
+	pub type AssetTransferCost<T: Config> = StorageMap<_, Blake2_128Concat, T::AssetId, T::Balance>;
 	
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		//// * `is_sufficient`: Whether this asset needs users to have an existential deposit to hold
+		/// Register a new asset in the asset manager.
+		///
+		/// * `origin`: Caller of this extrinsic, the acess control is specfied by `ForceOrigin`.
+		/// * `location`: Location of the asset.
+		/// * `metadata`: Asset metadata.
+		/// * `min_balance`: Minimum balance to keep an account alive, used in conjunction with `is_sufficient`. 
+		/// * `is_sufficient`: Whether this asset needs users to have an existential deposit to hold
 	    ///  this asset.
+		/// 
 		/// # <weight>
 		/// TODO: get actual weight
 		/// # </weight>
@@ -151,10 +192,61 @@ pub mod pallet {
 			origin: OriginFor<T>, 
 			location: T::AssetLocation,
 			metadata: T::AssetRegistrarMetadata,
-			min_balance: T::Balance,
-			is_sufficient: bool,
 		) -> DispatchResult {
 			T::ForceOrigin::ensure_origin(origin)?;
+			ensure!(
+				LocationAssetId::<T>::get(&location).is_none(),
+				Error::<T>::LocationAlreadyExists
+			);
+			let asset_id = Self::get_next_asset_id()?;
+			T::AssetRegistrar::create_asset(asset_id, metadata.min_balance(), metadata.clone().into(), metadata.is_sufficient()).map_err(|_| Error::<T>::ErrorCreatingAsset)?;
+			AssetIdLocation::<T>::insert(&asset_id, &location);
+			AssetIdMetadata::<T>::insert(&asset_id, &metadata.clone());
+			LocationAssetId::<T>::insert(&location, &asset_id);
+			Self::deposit_event(Event::<T>::AssetRegistered{asset_id, asset_address: location, metadata});
+			Ok(())
+		}
+
+		/// Update an asset by its asset id in the asset manager.
+		///
+		/// * `origin`: Caller of this extrinsic, the acess control is specfied by `ForceOrigin`.
+		/// * `asset_id`: AssetId to be updated.
+		/// * `metadata_option`: `Some(meta)` to update the metadata to `meta`, `None` means no update on metadata.
+		/// * `location_option`: `Some(location)` to update the asset location, `None` means no update on location.
+		/// 
+		/// # <weight>
+		/// TODO: get actual weight
+		/// # </weight>
+		#[pallet::weight(50_000_000)]
+		#[transactional]
+		pub fn update_asset(
+			origin: OriginFor<T>,
+			asset_id: T::AssetId,
+			location_option: Option<T::AssetLocation>,
+			metadata_option: Option<T::AssetRegistrarMetadata>
+		 ) -> DispatchResult {
+			// check validity.
+			T::ForceOrigin::ensure_origin(origin)?;
+			ensure!(
+				AssetIdLocation::<T>::contains_key(&asset_id),
+				Error::<T>::UpdateNonExistAsset
+			);
+			if let Some(location) = location_option.clone() {
+				ensure!(
+					!LocationAssetId::<T>::contains_key(&location),
+					Error::<T>::LocationAlreadyExists
+				)
+			}
+			// write to the ledger state.
+			if let Some(location) = location_option {
+				let old_location = AssetIdLocation::<T>::get(&asset_id).ok_or(Error::<T>::UpdateNonExistAsset)?;
+				LocationAssetId::<T>::remove(&old_location);
+				LocationAssetId::<T>::insert(&location, &asset_id);
+				AssetIdLocation::<T>::insert(&asset_id, &location);			
+			}
+			if let Some(metadata) = metadata_option {
+				AssetIdMetadata::<T>::insert(&asset_id, &metadata)
+			}
 			Ok(())
 		}
 
@@ -164,7 +256,7 @@ pub mod pallet {
 		fn get_next_asset_id() -> Result<T::AssetId, DispatchError> {
 			NextAssetId::<T>::try_mutate(|current| -> Result<T::AssetId, DispatchError> {
 				let id = *current;
-				*current = current.checked_add(One::one()).ok_or(ArithmeticError::Overflow)?;
+				*current = current.checked_add(&One::one()).ok_or(ArithmeticError::Overflow)?;
 				Ok(id)
 			})
 		}
