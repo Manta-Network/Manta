daed0feebaa9b981d463da90d5c376aecd6cee09
Daniel Ziabko <daniel399ziabko@gmail.com>
2022-01-23 12:27:32 -0500

    Added dolphin runtime & chainspec

---
+++ node/src/chain_specs/dolphin.rs
@@ -0,0 +1,231 @@
+// Copyright 2020-2022 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+
+use super::*;
+use crate::command::CALAMARI_PARACHAIN_ID;
+
+use calamari_runtime::{CouncilConfig, DemocracyConfig, GenesisConfig, TechnicalCommitteeConfig};
+
+/// Specialized `ChainSpec` for the normal parachain runtime.
+pub type CalamariChainSpec =
+	sc_service::GenericChainSpec<calamari_runtime::GenesisConfig, Extensions>;
+
+const DOLPHIN_PROTOCOL_ID: &str = "dolphin"; // for p2p network configuration
+const KUSAMA_RELAYCHAIN_LOCAL_NET: &str = "kusama-local";
+const KUSAMA_RELAYCHAIN_DEV_NET: &str = "kusama-dev";
+
+/// Generate the calamari session keys from individual elements.
+///
+/// The input must be a tuple of individual keys (a single arg for now since we have just one key).
+pub fn calamari_session_keys(keys: AuraId) -> calamari_runtime::opaque::SessionKeys {
+	calamari_runtime::opaque::SessionKeys { aura: keys }
+}
+
+// calamari chain specs
+pub fn calamari_properties() -> Properties {
+	let mut p = Properties::new();
+	p.insert("ss58format".into(), constants::CALAMARI_SS58PREFIX.into());
+	p.insert("tokenDecimals".into(), constants::CALAMARI_DECIMAL.into());
+	p.insert(
+		"tokenSymbol".into(),
+		constants::CALAMARI_TOKEN_SYMBOL.into(),
+	);
+	p
+}
+
+pub fn calamari_development_config() -> CalamariChainSpec {
+	let properties = calamari_properties();
+
+	CalamariChainSpec::from_genesis(
+		// Name
+		"Calamari Parachain Development",
+		// ID
+		"calamari_dev",
+		ChainType::Local,
+		move || {
+			calamari_dev_genesis(
+				// initial collators.
+				vec![(
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_collator_keys_from_seed("Alice"),
+				)],
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+				],
+			)
+		},
+		vec![],
+		None,
+		Some(CALAMARI_PROTOCOL_ID),
+		Some(properties),
+		Extensions {
+			relay_chain: KUSAMA_RELAYCHAIN_DEV_NET.into(),
+			para_id: CALAMARI_PARACHAIN_ID.into(),
+		},
+	)
+}
+
+pub fn calamari_local_config() -> CalamariChainSpec {
+	let properties = calamari_properties();
+
+	CalamariChainSpec::from_genesis(
+		// Name
+		"Calamari Parachain Local",
+		// ID
+		"calamari_local",
+		ChainType::Local,
+		move || {
+			calamari_dev_genesis(
+				// initial collators.
+				vec![
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						get_collator_keys_from_seed("Alice"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						get_collator_keys_from_seed("Bob"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Charlie"),
+						get_collator_keys_from_seed("Charlie"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Dave"),
+						get_collator_keys_from_seed("Dave"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Eve"),
+						get_collator_keys_from_seed("Eve"),
+					),
+				],
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+				],
+			)
+		},
+		vec![],
+		None,
+		Some(CALAMARI_PROTOCOL_ID),
+		Some(properties),
+		Extensions {
+			relay_chain: KUSAMA_RELAYCHAIN_LOCAL_NET.into(),
+			para_id: CALAMARI_PARACHAIN_ID.into(),
+		},
+	)
+}
+
+fn calamari_dev_genesis(
+	invulnerables: Vec<(AccountId, AuraId)>,
+	root_key: AccountId,
+	endowed_accounts: Vec<AccountId>,
+) -> calamari_runtime::GenesisConfig {
+	calamari_runtime::GenesisConfig {
+		system: calamari_runtime::SystemConfig {
+			code: calamari_runtime::WASM_BINARY
+				.expect("WASM binary was not build, please build it!")
+				.to_vec(),
+		},
+		balances: calamari_runtime::BalancesConfig {
+			balances: endowed_accounts[..endowed_accounts.len() / 2]
+				.iter()
+				.map(|k| {
+					(
+						k.clone(),
+						100 * CALAMARI_ENDOWMENT / ((endowed_accounts.len() / 2) as Balance),
+					)
+				})
+				.collect(),
+		},
+		// no need to pass anything to aura, in fact it will panic if we do. Session will take care
+		// of this.
+		aura: Default::default(),
+		sudo: calamari_runtime::SudoConfig { key: root_key },
+		parachain_info: calamari_runtime::ParachainInfoConfig {
+			parachain_id: CALAMARI_PARACHAIN_ID.into(),
+		},
+		collator_selection: calamari_runtime::CollatorSelectionConfig {
+			invulnerables: invulnerables.iter().cloned().map(|(acc, _)| acc).collect(),
+			candidacy_bond: KMA * 1000, // How many tokens will be reserved as collator
+			..Default::default()
+		},
+		session: calamari_runtime::SessionConfig {
+			keys: invulnerables
+				.iter()
+				.cloned()
+				.map(|(acc, aura)| {
+					(
+						acc.clone(),                 // account id
+						acc,                         // validator id
+						calamari_session_keys(aura), // session keys
+					)
+				})
+				.collect(),
+		},
+		democracy: DemocracyConfig::default(),
+		council: CouncilConfig {
+			members: endowed_accounts.iter().take(1).cloned().collect(),
+			phantom: Default::default(),
+		},
+		technical_committee: TechnicalCommitteeConfig {
+			members: endowed_accounts.iter().take(1).cloned().collect(),
+			phantom: Default::default(),
+		},
+		council_membership: Default::default(),
+		technical_membership: Default::default(),
+		aura_ext: Default::default(),
+		parachain_system: Default::default(),
+		polkadot_xcm: calamari_runtime::PolkadotXcmConfig {
+			safe_xcm_version: Some(0),
+		},
+	}
+}
+
+pub fn calamari_testnet_config() -> Result<CalamariChainSpec, String> {
+	let mut spec = CalamariChainSpec::from_json_bytes(
+		&include_bytes!("../../../genesis/calamari-testnet-genesis.json")[..],
+	)?;
+	spec.extensions_mut().para_id = CALAMARI_PARACHAIN_ID.into();
+	Ok(spec)
+}
+
+// Calamari testnet for ci jobs
+pub fn calamari_testnet_ci_config() -> Result<CalamariChainSpec, String> {
+	CalamariChainSpec::from_json_bytes(
+		&include_bytes!("../../../genesis/calamari-testnet-ci-genesis.json")[..],
+	)
+}
+
+// Calamari mainnet
+pub fn calamari_config() -> Result<CalamariChainSpec, String> {
+	CalamariChainSpec::from_json_bytes(
+		&include_bytes!("../../../genesis/calamari-genesis.json")[..],
+	)
+}
---
+++ runtime/dolphin/src/fee.rs
@@ -0,0 +1,69 @@
+// Copyright 2020-2022 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+
+use frame_support::weights::{
+	WeightToFeeCoefficient, WeightToFeeCoefficients, WeightToFeePolynomial,
+};
+use manta_primitives::Balance;
+use smallvec::smallvec;
+pub use sp_runtime::Perbill;
+
+/// The block saturation level. Fees will be updates based on this value.
+pub const TARGET_BLOCK_FULLNESS: Perbill = Perbill::from_percent(25);
+
+/// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
+/// node's balance type.
+///
+/// This should typically create a mapping between the following ranges:
+///   - [0, MAXIMUM_BLOCK_WEIGHT]
+///   - [Balance::min, Balance::max]
+///
+/// Yet, it can be used for any other sort of change to weight-fee. Some examples being:
+///   - Setting it to `0` will essentially disable the weight fee.
+///   - Setting it to `1` will cause the literal `#[weight = x]` values to be charged.
+pub struct WeightToFee;
+impl WeightToFeePolynomial for WeightToFee {
+	type Balance = Balance;
+	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
+		// Consider the daily cost to fully congest our network to be defined as:
+		// daily_cost_to_fully_congest = inclusion_fee * txs_per_block * blocks_per_day * dol_price
+		// The weight fee is defined as:
+		// weight_fee = coeff_integer * (weight ^ degree) + coeff_friction * (weight ^ degree)
+		// The inclusion fee is defined as:
+		// inclusion_fee = base_fee + length_fee + [targeted_fee_adjustment * weight_fee]
+		// As of the day of writing this code a single `balances.transfer` is benchmarked at 156626000 weight.
+		// Let's assume worst case scenarios where the `length_fee` of a transfer is negligible,
+		// and that `targeted_fee_adjustment` is simply 1, as if the network is not congested.
+		// Furthermore we know the `base_fee` is 0.000125DOL defined in our runtime. So:
+		// inclusion_fee = 0.000125 * coeff + 0.000156626 * coeff = 0.000281626 * coeff
+		// We have profiled `txs_per_block` to be around 1134 and `blocks_per_day` is known to be 7200.
+		// DOL price in dollars can be checked daily but at the time of writing the code it was $0.02. So:
+		// daily_cost_to_fully_congest = 0.000281626 * coeff * 1134 * 7200 * 0.02 = 45.988399296 * coeff
+		// Assuming we want the daily cost to be around $250000 we get:
+		// 250000 = 45.988399296 * coeff
+		// coeff = ~5436
+
+		// Keep in mind this is a rough worst-case scenario calculation.
+		// The `length_fee` could not be negligible, and the `targeted_fee_adjustment` will hike the fees
+		// as the network gets more and more congested, which will further increase the costs.
+		smallvec![WeightToFeeCoefficient {
+			coeff_integer: 5000u32.into(),
+			coeff_frac: Perbill::zero(),
+			negative: false,
+			degree: 1,
+		}]
+	}
+}
