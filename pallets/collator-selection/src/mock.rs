58fce01bde22d3dadf767550a976b1f7903eb322
ghzlatarev <ghzlatarev@gmail.com>
2022-02-02 22:20:27 +0200

    Fix compilation error

--- pallets/collator-selection/src/mock.rs
+++ pallets/collator-selection/src/mock.rs
@@ -79,6 +79,7 @@ impl system::Config for Test {
 	type SystemWeightInfo = ();
 	type SS58Prefix = SS58Prefix;
 	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
 parameter_types! {
