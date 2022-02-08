16cd96a238af884f7dcc27ef417369777c2e4176
ghzlatarev <ghzlatarev@gmail.com>
2022-02-08 07:15:31 +0200

    Runtime version should be 3130 and client version should bne 3.1.3

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -113,7 +113,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("calamari"),
 	impl_name: create_runtime_str!("calamari"),
 	authoring_version: 1,
-	spec_version: 3140,
+	spec_version: 3130,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
