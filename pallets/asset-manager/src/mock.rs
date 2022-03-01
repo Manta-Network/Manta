a9a6ae078a1b6d51e9be8cdd8568c4401ff91f74
Shumo Chu <stechu@users.noreply.github.com>
2022-03-01 08:53:42 -0500

    xcm and asset-manager (part 1) (#419)

---
+++ pallets/asset-manager/src/mock.rs
@@ -0,0 +1,196 @@
+// Copyright 2020-2022 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+//
+// The pallet-tx-pause pallet is forked from Acala's transaction-pause module https://github.com/AcalaNetwork/Acala/tree/master/modules/transaction-pause
+// The original license is the following - SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+//! Mock runtime for asset-manager
+
+use super::*;
+use crate as pallet_asset_manager;
+use frame_support::{construct_runtime, parameter_types, traits::ConstU32};
+use frame_system as system;
+use frame_system::EnsureRoot;
+use manta_primitives::{
+	AccountId, AssetId, AssetLocation, AssetRegistarMetadata, AssetStorageMetadata, Balance,
+	ASSET_STRING_LIMIT,
+};
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 78;
+}
+
+impl system::Config for Runtime {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<16>;
+}
+
+parameter_types! {
+	pub const AssetDeposit: Balance = 0; // Does not really matter as this will be only called by root
+	pub const AssetAccountDeposit: Balance = 0;
+	pub const ApprovalDeposit: Balance = 0;
+	pub const AssetsStringLimit: u32 = ASSET_STRING_LIMIT;
+	pub const MetadataDepositBase: Balance = 0;
+	pub const MetadataDepositPerByte: Balance = 0;
+}
+
+impl pallet_assets::Config for Runtime {
+	type Event = Event;
+	type Balance = Balance;
+	type AssetId = AssetId;
+	type Currency = Balances;
+	type ForceOrigin = EnsureRoot<AccountId>;
+	type AssetDeposit = AssetDeposit;
+	type AssetAccountDeposit = AssetAccountDeposit;
+	type MetadataDepositBase = MetadataDepositBase;
+	type MetadataDepositPerByte = MetadataDepositPerByte;
+	type ApprovalDeposit = ApprovalDeposit;
+	type StringLimit = AssetsStringLimit;
+	type Freezer = ();
+	type Extra = ();
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub ExistentialDeposit: Balance = 1;
+	pub const MaxLocks: u32 = 50;
+	pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Runtime {
+	type MaxLocks = MaxLocks;
+	type Balance = Balance;
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = [u8; 8];
+}
+
+pub struct AssetRegistrar;
+use frame_support::pallet_prelude::DispatchResult;
+impl pallet_asset_manager::AssetRegistrar<Runtime> for AssetRegistrar {
+	fn create_asset(
+		asset_id: AssetId,
+		min_balance: Balance,
+		metadata: AssetStorageMetadata,
+		is_sufficient: bool,
+	) -> DispatchResult {
+		Assets::force_create(
+			Origin::root(),
+			asset_id,
+			AssetManager::account_id(),
+			is_sufficient,
+			min_balance,
+		)?;
+
+		Assets::force_set_metadata(
+			Origin::root(),
+			asset_id,
+			metadata.name,
+			metadata.symbol,
+			metadata.decimals,
+			metadata.is_frozen,
+		)
+	}
+
+	fn update_asset_metadata(asset_id: AssetId, metadata: AssetStorageMetadata) -> DispatchResult {
+		Assets::force_set_metadata(
+			Origin::root(),
+			asset_id,
+			metadata.name,
+			metadata.symbol,
+			metadata.decimals,
+			metadata.is_frozen,
+		)
+	}
+}
+
+impl AssetMetadata<Runtime> for AssetRegistarMetadata<Balance> {
+	fn min_balance(&self) -> Balance {
+		self.min_balance
+	}
+
+	fn is_sufficient(&self) -> bool {
+		self.is_sufficient
+	}
+}
+
+impl pallet_asset_manager::Config for Runtime {
+	type Event = Event;
+	type Balance = Balance;
+	type AssetId = AssetId;
+	type AssetRegistrarMetadata = AssetRegistarMetadata<Balance>;
+	type StorageMetadata = AssetStorageMetadata;
+	type AssetLocation = AssetLocation;
+	type AssetRegistrar = AssetRegistrar;
+	type ModifierOrigin = EnsureRoot<AccountId>;
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Storage, Config, Event<T>} = 0,
+		Assets: pallet_assets::{Pallet, Storage, Event<T>} = 1,
+		AssetManager: pallet_asset_manager::{Pallet, Call, Storage, Event<T>} = 2,
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>} = 3,
+	}
+);
+
+pub const PALLET_BALANCES_INDEX: u8 = 3;
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let t = frame_system::GenesisConfig::default()
+		.build_storage::<Runtime>()
+		.unwrap();
+	sp_io::TestExternalities::new(t)
+}
---
+++ pallets/asset-manager/src/tests.rs
@@ -0,0 +1,191 @@
+// Copyright 2020-2022 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+//
+// The pallet-tx-pause pallet is forked from Acala's transaction-pause module https://github.com/AcalaNetwork/Acala/tree/master/modules/transaction-pause
+// The original license is the following - SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+//! unit tests for asset-manager
+
+use crate::{self as asset_manager, AssetIdLocation, UnitsPerSecond};
+use asset_manager::mock::*;
+use frame_support::{assert_noop, assert_ok};
+use manta_primitives::{AssetLocation, AssetRegistarMetadata};
+use sp_runtime::traits::BadOrigin;
+use xcm::{latest::prelude::*, VersionedMultiLocation};
+
+#[test]
+fn basic_setup_should_work() {
+	new_test_ext()
+		.execute_with(|| assert!(AssetIdLocation::<Runtime>::iter_values().next().is_none()));
+}
+
+#[test]
+fn wrong_modifer_origin_should_not_work() {
+	new_test_ext().execute_with(|| {
+		let asset_metadata = AssetRegistarMetadata {
+			name: b"Kusama".to_vec(),
+			symbol: b"KSM".to_vec(),
+			decimals: 12,
+			min_balance: 1u128,
+			evm_address: None,
+			is_frozen: false,
+			is_sufficient: true,
+		};
+		let source_location = AssetLocation(VersionedMultiLocation::V1(MultiLocation::parent()));
+		assert_noop!(
+			AssetManager::register_asset(
+				Origin::signed([0u8; 32].into()),
+				source_location.clone(),
+				asset_metadata.clone()
+			),
+			BadOrigin
+		);
+		assert_noop!(
+			AssetManager::update_asset_location(
+				Origin::signed([2u8; 32].into()),
+				0,
+				source_location
+			),
+			BadOrigin
+		);
+		assert_noop!(
+			AssetManager::update_asset_metadata(
+				Origin::signed([3u8; 32].into()),
+				0,
+				asset_metadata
+			),
+			BadOrigin
+		);
+		assert_noop!(
+			AssetManager::set_units_per_second(Origin::signed([4u8; 32].into()), 0, 0),
+			BadOrigin
+		);
+	})
+}
+
+#[test]
+fn register_asset_should_work() {
+	let asset_metadata = AssetRegistarMetadata {
+		name: b"Kusama".to_vec(),
+		symbol: b"KSM".to_vec(),
+		decimals: 12,
+		min_balance: 1u128,
+		evm_address: None,
+		is_frozen: false,
+		is_sufficient: true,
+	};
+	let source_location = AssetLocation(VersionedMultiLocation::V1(MultiLocation::parent()));
+	let new_location = AssetLocation(VersionedMultiLocation::V1(MultiLocation::new(
+		1,
+		X2(Parachain(1), PalletInstance(PALLET_BALANCES_INDEX)),
+	)));
+	new_test_ext().execute_with(|| {
+		let mut counter: u32 = 0;
+		// Register relay chain native token
+		assert_ok!(AssetManager::register_asset(
+			Origin::root(),
+			source_location.clone(),
+			asset_metadata.clone()
+		));
+		assert_eq!(
+			AssetIdLocation::<Runtime>::get(counter),
+			Some(source_location.clone())
+		);
+		counter += 1;
+		// Register twice will fail
+		assert_noop!(
+			AssetManager::register_asset(Origin::root(), source_location, asset_metadata.clone()),
+			crate::Error::<Runtime>::LocationAlreadyExists
+		);
+		// Register a new asset
+		assert_ok!(AssetManager::register_asset(
+			Origin::root(),
+			new_location.clone(),
+			asset_metadata.clone()
+		));
+		assert_eq!(AssetIdLocation::<Runtime>::get(counter), Some(new_location));
+	})
+}
+
+#[test]
+fn update_asset() {
+	let asset_metadata = AssetRegistarMetadata {
+		name: b"Kusama".to_vec(),
+		symbol: b"KSM".to_vec(),
+		decimals: 12,
+		min_balance: 1u128,
+		evm_address: None,
+		is_frozen: false,
+		is_sufficient: true,
+	};
+	let mut new_metadata = asset_metadata.clone();
+	new_metadata.is_frozen = true;
+	let source_location = AssetLocation(VersionedMultiLocation::V1(MultiLocation::parent()));
+	let new_location = AssetLocation(VersionedMultiLocation::V1(MultiLocation::new(
+		1,
+		X2(Parachain(1), PalletInstance(PALLET_BALANCES_INDEX)),
+	)));
+	new_test_ext().execute_with(|| {
+		// Register relay chain native token
+		assert_ok!(AssetManager::register_asset(
+			Origin::root(),
+			source_location.clone(),
+			asset_metadata.clone()
+		));
+		assert_eq!(
+			AssetIdLocation::<Runtime>::get(0),
+			Some(source_location.clone())
+		);
+		// Update the asset metadata
+		assert_ok!(AssetManager::update_asset_metadata(
+			Origin::root(),
+			0,
+			new_metadata.clone()
+		));
+		// Update the asset location
+		assert_ok!(AssetManager::update_asset_location(
+			Origin::root(),
+			0,
+			new_location.clone()
+		));
+		// Update asset units per seconds
+		assert_ok!(AssetManager::set_units_per_second(
+			Origin::root(),
+			0,
+			125u128
+		));
+		assert_eq!(UnitsPerSecond::<Runtime>::get(0), Some(125));
+		// Update a non-exist asset should fail
+		assert_noop!(
+			AssetManager::update_asset_location(Origin::root(), 1, new_location.clone()),
+			crate::Error::<Runtime>::UpdateNonExistAsset
+		);
+		assert_noop!(
+			AssetManager::update_asset_metadata(Origin::root(), 1, new_metadata.clone()),
+			crate::Error::<Runtime>::UpdateNonExistAsset
+		);
+		// Update an asset to an existing location will fail
+		assert_ok!(AssetManager::register_asset(
+			Origin::root(),
+			source_location.clone(),
+			asset_metadata.clone()
+		));
+		assert_noop!(
+			AssetManager::update_asset_location(Origin::root(), 1, new_location),
+			crate::Error::<Runtime>::LocationAlreadyExists
+		);
+	})
+}
