d488b5df262d9d75a206ad42ca62b242a116b811
Daniel Ziabko <daniel399ziabko@gmail.com>
2022-03-08 10:43:08 -0500

    Added SlowAdjustingFeeUpdate & congested_chain_simulation test for Dolphin.
    Signed-off-by: Daniel Ziabko <daniel399ziabko@gmail.com>

--- runtime/dolphin/src/lib.rs
+++ runtime/dolphin/src/lib.rs
@@ -58,7 +58,7 @@ use manta_primitives::{
 	constants::time::*,
 	types::{AccountId, AuraId, Balance, BlockNumber, Hash, Header, Index, Signature},
 };
-use sp_runtime::{Perbill, Permill};
+use sp_runtime::{Perbill, Permill, Perquintill, FixedPointNumber};
 
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
@@ -66,7 +66,7 @@ pub use sp_runtime::BuildStorage;
 // Polkadot imports
 use pallet_xcm::XcmPassthrough;
 use polkadot_parachain::primitives::Sibling;
-use polkadot_runtime_common::{BlockHashCount, RocksDbWeight, SlowAdjustingFeeUpdate};
+use polkadot_runtime_common::{BlockHashCount, RocksDbWeight};
 use xcm::latest::prelude::*;
 use xcm_builder::{
 	AccountId32Aliases, AllowKnownQueryResponses, AllowSubscriptionsFrom,
@@ -76,6 +76,7 @@ use xcm_builder::{
 	SignedAccountId32AsNative, SovereignSignedViaLocation, TakeWeightCredit, UsingComponents,
 };
 use xcm_executor::{Config, XcmExecutor};
+use pallet_transaction_payment::{Multiplier, TargetedFeeAdjustment};
 
 pub mod currency;
 pub mod fee;
@@ -904,6 +905,26 @@ pub type Executive = frame_executive::Executive<
 	Runtime,
 	AllPalletsReversedWithSystemFirst,
 >;
+
+// Common constants used in all runtimes.
+parameter_types! {
+	/// The portion of the `NORMAL_DISPATCH_RATIO` that we adjust the fees with. Blocks filled less
+	/// than this will decrease the weight and more will increase.
+	pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+	/// The adjustment variable of the runtime. Higher values will cause `TargetBlockFullness` to
+	/// change the fees more rapidly.
+	pub AdjustmentVariable: Multiplier = Multiplier::saturating_from_rational(3, 100_000);
+	/// Minimum amount of the multiplier. This value cannot be too low. A test case should ensure
+	/// that combined with `AdjustmentVariable`, we can recover from the minimum.
+	/// See `multiplier_can_grow_from_zero`.
+	pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1, 1_000_000u128);
+}
+
+/// Parameterized slow adjusting fee updated based on
+/// https://research.web3.foundation/en/latest/polkadot/overview/2-token-economics.html#-2.-slow-adjusting-mechanism
+pub type SlowAdjustingFeeUpdate<R> =
+	TargetedFeeAdjustment<R, TargetBlockFullness, AdjustmentVariable, MinimumMultiplier>;
+
 
 impl_runtime_apis! {
 	impl sp_consensus_aura::AuraApi<Block, AuraId> for Runtime {
