e0af50deaaeea0d6dc0f1f728c527e9a51acf378
Adam Reif <Garandor@manta.network>
2022-03-25 03:30:37 -0500

    Revert spec_version bump
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -116,7 +116,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("calamari"),
 	impl_name: create_runtime_str!("calamari"),
 	authoring_version: 1,
-	spec_version: 3142,
+	spec_version: 3141,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
