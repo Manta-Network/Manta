8bb21d8dbeae17fe59ee495a4c18c7ef217604f3
ghzlatarev <ghzlatarev@gmail.com>
2022-04-13 14:53:08 +0300

    Add missing Assets, AssetManager and MantaPay weights to benchmarking CI workflow for Dolphin and Calamari
    Signed-off-by: ghzlatarev <ghzlatarev@gmail.com>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -1319,6 +1319,7 @@ impl_runtime_apis! {
 			list_benchmark!(list, extra, pallet_session, SessionBench::<Runtime>);
 			list_benchmark!(list, extra, pallet_tx_pause, TransactionPause);
 			list_benchmark!(list, extra, pallet_assets, Assets);
+			list_benchmark!(list, extra, pallet_asset_manager, AssetManager);
 
 			let storage_info = AllPalletsReversedWithSystemFirst::storage_info();
 
@@ -1369,6 +1370,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, pallet_session, SessionBench::<Runtime>);
 			add_benchmark!(params, batches, pallet_tx_pause, TransactionPause);
 			add_benchmark!(params, batches, pallet_assets, Assets);
+			add_benchmark!(params, batches, pallet_asset_manager, AssetManager);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
