9efb1de7da94c226a647101d1f2dafbe02c6ea64
Dengjianping <djptux@gmail.com>
2022-05-20 20:13:19 +0800

    3/5 for transaction pause
    Signed-off-by: Dengjianping <djptux@gmail.com>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -162,7 +162,7 @@ impl pallet_tx_pause::Config for Runtime {
 	type Event = Event;
 	type UpdateOrigin = EnsureOneOf<
 		EnsureRoot<AccountId>,
-		pallet_collective::EnsureProportionAtLeast<_2, _3, AccountId, TechnicalCollective>,
+		pallet_collective::EnsureProportionAtLeast<_3, _5, AccountId, TechnicalCollective>,
 	>;
 	type WeightInfo = weights::pallet_tx_pause::SubstrateWeight<Runtime>;
 }
