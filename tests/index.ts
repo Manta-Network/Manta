3b0848ef09a8461f754e56a4a5e1da42dfc8a9ee
Georgi Zlatarev <georgi.zlatarev@manta.network>
2022-06-16 22:29:00 +0300

    Add cases
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- tests/index.ts
+++ tests/index.ts
@@ -114,33 +114,40 @@ async function main(){
     const shards_amount = 3;
     const utxo_per_shard_amount = 3;
 
+    let next = 0;
     for(var i = 0; i < shards_amount; ++i) {
         for(var j = 0; j < utxo_per_shard_amount; ++j) {
             var shard_index: u8 = (i as unknown) as u8;
             var element_in_shard_index: bigint = (j as unknown) as bigint;
             const shards_storage_key = shard_storage_key(shard_index, element_in_shard_index);
             console.log('Shards storage key for shard_index: ' + shard_index + ' and element_index: ' + element_in_shard_index + ' is: ' + shards_storage_key);
-    
+            // var next_element = (i * 2 + j).toString();
+            let next_element_string = next.toString();
+            for(var k = next_element_string.length; k < 264; ++k) {
+                next_element_string = '0' + next_element_string;
+            }
+            next_element_string = '0x' + next_element_string;
             const shards_call = api.tx.system.setStorage([
                 [ 
                     shards_storage_key,
                     // Insert some random but correct value:
-                    '0x83590b405cf760cb1660fc295f7810d428fb27d946f2bba38cb9ca5b7d4ed643c5e56ae65158f96c93573210b6a0f36eadf01166b77dbe49247947f669daa1225e11b47dd076bf70568bd8d9ceb93a90e49ba1ce0a651f2a0107364da1d2f018776494b592a8eb26b8af06fb56e681e3efadd4d23f12eedac960fdeb455f66fbeb0967bf']
+                    //'0x83590b405cf760cb1660fc295f7810d428fb27d946f2bba38cb9ca5b7d4ed643c5e56ae65158f96c93573210b6a0f36eadf01166b77dbe49247947f669daa1225e11b47dd076bf70568bd8d9ceb93a90e49ba1ce0a651f2a0107364da1d2f018776494b592a8eb26b8af06fb56e681e3efadd4d23f12eedac960fdeb455f66fbeb0967bf']
+                    next_element_string
                 ]
-            );
-    
+            ]);
+            next++;
             await api.tx.sudo.sudo(shards_call).signAndSend(sudo_key_pair, ({ events = [], status }) => {
-                console.log('Call status:', status.type);
-                if (status.isInBlock) {
-                    console.error('You have just set storage for MantaPay Shards on your chain');
+                // console.log('Call status:', status.type);
+                // if (status.isInBlock) {
+                //     console.error('You have just set storage for MantaPay Shards on your chain');
         
-                    console.log('Included at block hash', status.asInBlock.toHex());
-                    console.log('Events:');
+                //     console.log('Included at block hash', status.asInBlock.toHex());
+                //     console.log('Events:');
         
-                    console.log(JSON.stringify(events, null, 2));
-                } else if (status.isFinalized) {
-                    console.log('Finalized block hash', status.asFinalized.toHex());
-                }
+                //     console.log(JSON.stringify(events, null, 2));
+                // } else if (status.isFinalized) {
+                //     console.log('Finalized block hash', status.asFinalized.toHex());
+                // }
             });
     
             wait(20000);
@@ -152,30 +159,110 @@ async function main(){
         var vsio_element_index: bigint = (i as unknown) as bigint;
         const vsio_storage_key = vn_insertion_order_set_storage_key(vsio_element_index);
         console.log('VoidNumberSetInsertionOrder storage key for index: ' + vsio_element_index + ' is: ' + vsio_storage_key);
-    
+        var next_element_string = i.toString();
+        for(var k = next_element_string.length; k < 64; ++k) {
+            next_element_string = '0' + next_element_string;
+        }
+        next_element_string = '0x' + next_element_string;
+        // console.log(next_element);
         // Insert some random but correct value:
-        const call = api.tx.system.setStorage([[vsio_storage_key, '0xefe34cfd4418c9b1c04e555965e479d00ec4814ed0cd94641df1a8c6f9fa1071']]);
+        // '0xefe34cfd4418c9b1c04e555965e479d00ec4814ed0cd94641df1a8c6f9fa1071'
+        const call = api.tx.system.setStorage([[vsio_storage_key, next_element_string]]);
         await api.tx.sudo.sudo(call).signAndSend(sudo_key_pair, ({ events = [], status }) => {
-            console.log('Call status:', status.type);
-            if (status.isInBlock) {
-                console.error('You have just set storage of VoidNumberSetInsertionOrder on your chain');
+            // console.log('Call status:', status.type);
+            // if (status.isInBlock) {
+            //     console.error('You have just set storage of VoidNumberSetInsertionOrder on your chain');
         
-                console.log('Included at block hash', status.asInBlock.toHex());
-                console.log('Events:');
+            //     console.log('Included at block hash', status.asInBlock.toHex());
+            //     console.log('Events:');
         
-                console.log(JSON.stringify(events, null, 2));
-            } else if (status.isFinalized) {
-                console.log('Finalized block hash', status.asFinalized.toHex());
-            }
+            //     console.log(JSON.stringify(events, null, 2));
+            // } else if (status.isFinalized) {
+            //     console.log('Finalized block hash', status.asFinalized.toHex());
+            // }
         });
         
         wait(20000);    
     }
 
     wait(60000);
+    
+    // receiver_index [0, 0, 0, ...] -> gets all 9 receivers
+    // receiver_index [1, 1, 1, ...] -> gets all 6 receivers
+    // receiver_index [2, 2, 2, ...] -> gets all 3 receivers
+    // receiver_index [3, 3, 3, ...] -> gets all 0 receivers
+    var next_receiver_num = -1;
+    var next_sender_num = -1;
+    for(var k = 0; k < 3; ++k) {
+        for(var j = 0; j < 3; ++j) {
+            for(var i = 0; i < 3; ++i) {
+                var receiver_index = new Array(256).fill(0);
+                receiver_index[0] = i;
+                receiver_index[1] = j;
+                receiver_index[2] = k;
+                const expected_entries = 9 - i - j - k;
+                const payload = await (api.rpc as any).mantaPay.pull_ledger_diff({receiver_index, sender_index: i + j + k});
+                let [should_continue, receivers, senders] = payload;
+                if(should_continue == true) {
+                    console.log("Test Failed!");
+                    console.log("receiver_index: " + receiver_index);
+                    console.log("should continue should be false");
+                }
+                if(receivers[1].length != expected_entries) {
+                    console.log("Test Failed!");
+                    console.log("receiver_index: " + receiver_index);
+                    console.log("receivers expected_entries: " + expected_entries);
+                    console.log("receivers length: " + receivers[1].length);
+                } else {
+                    let [some, [ek, ct]] = receivers[1][0];
+                    var prev = parseInt(ct[1].toString());
+                    for(var l = 1; l < receivers[1].length; ++l) {
+                        let [some, [ek, ct]] = receivers[1][l];
+                        let next = parseInt(ct[1].toString());
+                        if(next <= prev) {
+                            console.log("Test Failed!");
+                            console.log("prev: " + prev);
+                            console.log("next: " + next);
+                        }
+                        prev = next;
+                    }
+                }
+                if(senders[1].length != expected_entries) {
+                    console.log("Test Failed!");
+                    console.log("receiver_index: " + receiver_index);
+                    console.log("senders expected_entries: " + expected_entries);
+                    console.log("senders length: " + senders[1].length);
+                } else {
+                    var prev = parseInt(senders[1][0].toString());
+                    for(var l = 1; l < senders[1].length; ++l) {
+                        let next = parseInt(senders[1][l].toString());
+                        if(next <= prev) {
+                            console.log("Test Failed!");
+                            console.log("prev: " + prev);
+                            console.log("next: " + next);
+                        }
+                        prev = next;
+                    }
+                }
+            }
+        }
+    }
 
-    const payload = await (api.rpc as any).mantaPay.pull_ledger_diff({receiver_index: new Array(256).fill(0), sender_index: 0});
-    console.log(payload.toHuman());
+    var receiver_index = new Array(256).fill(3);
+    var payload = await (api.rpc as any).mantaPay.pull_ledger_diff({receiver_index, sender_index: 9});
+    let [should_continue, receivers, senders] = payload;
+    if(receivers[1].length != 0 || senders[1].length != 0) {
+        console.log("Test Failed!");
+        console.log("receiver_index: " + receiver_index);
+        console.log("senders expected_entries: " + 0);
+        console.log("receivers length: " + receivers[1].length);
+        console.log("senders length: " + senders[1].length);
+    }
+
+    var sc = true;
+    while(sc) {
+        var payload = await (api.rpc as any).mantaPay.pull_ledger_diff({receiver_index, sender_index: 9});
+    }
 }
 
 main().catch(console.error).finally(() => process.exit());
