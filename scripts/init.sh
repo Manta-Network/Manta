ede269bc809f43a30b629833a8078f921878d0e0
Adam Reif <Garandor@manta.network>
2022-07-12 08:40:41 -0500

    Pin 07-10 rust nightly in CI
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,4 +3,4 @@
 rustup default stable
 rustup update
 rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly-2022-07-10-x86_64-unknown-linux-gnu
