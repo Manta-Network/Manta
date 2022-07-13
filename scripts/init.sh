822aca103d6f3bebfe9fac3636a674a95ea494dd
Adam Reif <Garandor@manta.network>
2022-07-12 19:19:38 -0500

    revert init.sh change
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rustup default 1.61.0
+rustup default stable
 rustup update
 rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-07-10-x86_64-unknown-linux-gnu
+rustup target add wasm32-unknown-unknown --toolchain nightly
