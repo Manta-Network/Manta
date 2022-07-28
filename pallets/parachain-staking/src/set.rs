7ec0839959949f72214480ab638cae2e9b407a7a
Adam Reif <Garandor@manta.network>
2022-07-28 13:16:18 -0500

    clippy and format
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- pallets/parachain-staking/src/set.rs
+++ pallets/parachain-staking/src/set.rs
@@ -28,62 +28,62 @@ use sp_std::prelude::*;
 pub struct OrderedSet<T>(pub Vec<T>);
 
 impl<T: Ord> OrderedSet<T> {
-	/// Create a new empty set
-	pub fn new() -> Self {
-		Self(Vec::new())
-	}
+    /// Create a new empty set
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
 
-	/// Create a set from a `Vec`.
-	/// `v` will be sorted and dedup first.
-	pub fn from(mut v: Vec<T>) -> Self {
-		v.sort();
-		v.dedup();
-		Self::from_sorted_set(v)
-	}
+    /// Create a set from a `Vec`.
+    /// `v` will be sorted and dedup first.
+    pub fn from(mut v: Vec<T>) -> Self {
+        v.sort();
+        v.dedup();
+        Self::from_sorted_set(v)
+    }
 
-	/// Create a set from a `Vec`.
-	/// Assume `v` is sorted and contain unique elements.
-	pub fn from_sorted_set(v: Vec<T>) -> Self {
-		Self(v)
-	}
+    /// Create a set from a `Vec`.
+    /// Assume `v` is sorted and contain unique elements.
+    pub fn from_sorted_set(v: Vec<T>) -> Self {
+        Self(v)
+    }
 
-	/// Insert an element.
-	/// Return true if insertion happened.
-	pub fn insert(&mut self, value: T) -> bool {
-		match self.0.binary_search(&value) {
-			Ok(_) => false,
-			Err(loc) => {
-				self.0.insert(loc, value);
-				true
-			}
-		}
-	}
+    /// Insert an element.
+    /// Return true if insertion happened.
+    pub fn insert(&mut self, value: T) -> bool {
+        match self.0.binary_search(&value) {
+            Ok(_) => false,
+            Err(loc) => {
+                self.0.insert(loc, value);
+                true
+            }
+        }
+    }
 
-	/// Remove an element.
-	/// Return true if removal happened.
-	pub fn remove(&mut self, value: &T) -> bool {
-		match self.0.binary_search(value) {
-			Ok(loc) => {
-				self.0.remove(loc);
-				true
-			}
-			Err(_) => false,
-		}
-	}
+    /// Remove an element.
+    /// Return true if removal happened.
+    pub fn remove(&mut self, value: &T) -> bool {
+        match self.0.binary_search(value) {
+            Ok(loc) => {
+                self.0.remove(loc);
+                true
+            }
+            Err(_) => false,
+        }
+    }
 
-	/// Return if the set contains `value`
-	pub fn contains(&self, value: &T) -> bool {
-		self.0.binary_search(value).is_ok()
-	}
+    /// Return if the set contains `value`
+    pub fn contains(&self, value: &T) -> bool {
+        self.0.binary_search(value).is_ok()
+    }
 
-	/// Clear the set
-	pub fn clear(&mut self) {
-		self.0.clear();
-	}
+    /// Clear the set
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
 }
 
 impl<T: Ord> From<Vec<T>> for OrderedSet<T> {
-	fn from(v: Vec<T>) -> Self {
-		Self::from(v)
-	}
+    fn from(v: Vec<T>) -> Self {
+        Self::from(v)
+    }
 }
--- pallets/parachain-staking/src/traits.rs
+++ pallets/parachain-staking/src/traits.rs
@@ -17,27 +17,27 @@
 //! traits for parachain-staking
 
 pub trait OnCollatorPayout<AccountId, Balance> {
-	fn on_collator_payout(
-		for_round: crate::RoundIndex,
-		collator_id: AccountId,
-		amount: Balance,
-	) -> frame_support::pallet_prelude::Weight;
+    fn on_collator_payout(
+        for_round: crate::RoundIndex,
+        collator_id: AccountId,
+        amount: Balance,
+    ) -> frame_support::pallet_prelude::Weight;
 }
 impl<AccountId, Balance> OnCollatorPayout<AccountId, Balance> for () {
-	fn on_collator_payout(
-		_for_round: crate::RoundIndex,
-		_collator_id: AccountId,
-		_amount: Balance,
-	) -> frame_support::pallet_prelude::Weight {
-		0
-	}
+    fn on_collator_payout(
+        _for_round: crate::RoundIndex,
+        _collator_id: AccountId,
+        _amount: Balance,
+    ) -> frame_support::pallet_prelude::Weight {
+        0
+    }
 }
 
 pub trait OnNewRound {
-	fn on_new_round(round_index: crate::RoundIndex) -> frame_support::pallet_prelude::Weight;
+    fn on_new_round(round_index: crate::RoundIndex) -> frame_support::pallet_prelude::Weight;
 }
 impl OnNewRound for () {
-	fn on_new_round(_round_index: crate::RoundIndex) -> frame_support::pallet_prelude::Weight {
-		0
-	}
+    fn on_new_round(_round_index: crate::RoundIndex) -> frame_support::pallet_prelude::Weight {
+        0
+    }
 }
