106145ecbdab42c834bf49cbbf150b1375f67eb3
Adam Reif <Garandor@manta.network>
2022-09-14 12:17:40 -0500

    fixup include
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- runtime/calamari/src/migrations/staking.rs
+++ runtime/calamari/src/migrations/staking.rs
@@ -96,7 +96,7 @@ where
 
     #[cfg(feature = "try-runtime")]
     fn pre_upgrade() -> Result<(), &'static str> {
-        use sp_api_hidden_includes_construct_runtime::hidden_include::traits::Currency;
+        use crate::sp_api_hidden_includes_construct_runtime::hidden_include::traits::Currency;
 
         // 2. Ensure they each have 400k KMA
         let invulnerables = manta_collator_selection::Pallet::<T>::invulnerables();
