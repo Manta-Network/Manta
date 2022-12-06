cd67599ec5a0c21bf74aec023594b8193a31101f
zqhxuyuan <zqhxuyuan@gmail.com>
2022-12-06 16:21:11 +0800

    fix proof encode because its size isnot u128
    Signed-off-by: zqhxuyuan <zqhxuyuan@gmail.com>

--- pallets/manta-pay/src/types.rs
+++ pallets/manta-pay/src/types.rs
@@ -107,9 +107,17 @@ where
     T: PairingEngine,
 {
     use manta_util::codec::Encode;
-    proof_as_bytes(&proof)
-        .try_into()
-        .map_err(|_e| scale_codec::Error::from(PROOF_ENCODE))
+    let bytes = proof.to_vec();
+    if bytes.len() > 128 {
+        let u128_bytes = &bytes[8..];
+        let vec = u128_bytes.to_vec();
+        vec.try_into()
+            .map_err(|_e| scale_codec::Error::from(PROOF_ENCODE))
+    } else {
+        bytes
+            .try_into()
+            .map_err(|_e| scale_codec::Error::from(PROOF_ENCODE))
+    }
 }
 
 /// Proof decode from byte array
