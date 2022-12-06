5d69a4b121cabb02608c9e85287f44dc5f63b897
Apokalip <simeon@manta.network>
2022-12-06 21:34:11 +0200

    format
    Signed-off-by: Apokalip <simeon@manta.network>
    Signed-off-by: zqhxuyuan <zqhxuyuan@gmail.com>

--- pallets/manta-pay/src/benchmark/mod.rs
+++ pallets/manta-pay/src/benchmark/mod.rs
@@ -18,8 +18,7 @@ use crate::{
     benchmark::precomputed_coins::{
         PRIVATE_TRANSFER, PRIVATE_TRANSFER_INPUT, TO_PRIVATE, TO_PUBLIC, TO_PUBLIC_INPUT,
     },
-    types::Asset,
-    types::{asset_value_decode, asset_value_encode},
+    types::{asset_value_decode, asset_value_encode, Asset},
     Call, Config, Event, Pallet, StandardAssetId, TransferPost,
 };
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
