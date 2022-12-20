677bd5f62535ab2e17dee2d54b53719a6f28d0e2
Georgi Zlatarev <georgi.zlatarev@manta.network>
2022-12-21 00:06:21 +0200

    Hard code prefixes
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- tests/append_storage.js
+++ tests/append_storage.js
@@ -1,33 +1,12 @@
 const fs = require('fs');
-const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
-const { xxhashAsHex } = require('@polkadot/util-crypto');
 
-let prefixes = ['0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9' /* System.Account */];
-const skippedModulesPrefix = ['System', 'Session', 'Authorship', 'CollatorSelection', 'ParachainSystem', 'Timestamp','ParachainInfo', 
-'TransactionPause', 'TransactionPayment', 'Democracy', 'Council', 'CouncilMembership','TechnicalCommittee', 'AuthorInherent', 'AuraAuthorFilter',
-'Aura', 'Treasury', 'XcmpQueue', 'PolkadotXcm', 'CumulusXcm', 'DmpQueue', 'XTokens', 'Utility', 'Multisig', 'Preimage', 'Scheduler', 'Sudo', 
-'TechnicalMembership', 'AuraExt', 'ParachainStaking', 'AuthorInherent', 'AuraAuthorFilter', 'CalamariVesting', 'Balances',]
+let prefixes = ['0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9',
+'0x682a59d51ab9e48a8c8cc418ff9708d2',
+'0x4ae7e256f92e5888372d72f3e4db1003',
+'0xa66d1aecfdbd14d785a4d1d8723b4beb'
+];
 
 async function main() {
-  let nodeAddress = "ws://127.0.0.1:9801";
-
-  const wsProvider = new WsProvider(nodeAddress);
-  const api = await ApiPromise.create({ 
-      provider: wsProvider});
-
-  const metadata = await api.rpc.state.getMetadata();
-  // Populate the prefixes array
-  const modules = metadata.asLatest.pallets;
-  modules.forEach((module) => {
-    if (module.storage) {
-      if (!skippedModulesPrefix.includes(module.name.toHuman())) {
-        prefixes.push(xxhashAsHex(module.name, 128));
-        console.log("\n module.name: ", module.name);
-      }
-    }
-  });
-  console.log("\n prefixes: ", prefixes);
-
   let storage = JSON.parse(fs.readFileSync('./storage.json', 'utf8'));
   let forkedSpec = JSON.parse(fs.readFileSync('./fork.json', 'utf8'));
 
