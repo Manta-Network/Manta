6882f79893d2a8c5173508dd33aecbf40fc1a99f
Georgi Zlatarev <georgi.zlatarev@manta.network>
2022-12-21 15:49:11 +0200

    Clean up
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- tests/stress_benchmark_test.ts
+++ tests/stress_benchmark_test.ts
@@ -4,8 +4,6 @@ import { manta_pay_types, rpc_api } from './types';
 import { delay } from './test-util';
 import { assert } from 'chai';
 import minimist, { ParsedArgs } from 'minimist';
-import { blake2AsHex } from "@polkadot/util-crypto";
-import * as fs from 'fs';
 import { readFile } from 'fs/promises';
 
 const test_config = {
@@ -13,6 +11,15 @@ const test_config = {
     mnemonic: 'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice',
     timeout: 20000000,
     max_wait_time_sec: 100000,
+    mints_offset: 2,
+    transfers_offset: 4,
+    reclaims_offset: 4,
+    total_iterations: 15000,
+    start_iteration: 13500,
+    tests_iterations: 100,
+    mint_size: 100,
+    transfer_size: 100,
+    reclaim_size: 100,
     expected_tps: 1.9
 }
  
@@ -35,42 +42,32 @@ describe('Node RPC Test', () => {
             rpc: rpc_api});
         const keyring = new Keyring({ type: 'sr25519' });
         const sender = keyring.addFromMnemonic(test_config.mnemonic);
-       
-        let mints_offset = 2;
-        let transfers_offset = 4;
-        let reclaims_offset = 4;
-        let total_iterations = 15000;
-        let start_iteration = 13500;
-        let tests_iterations = 100;
 
-        let mint_size = 552;
         const mints_content = await readFile("./precomputed_mints");
         const mints_buffer = mints_content.subarray(
-          mints_offset,
-          mint_size * total_iterations
+          test_config.mints_offset,
+          test_config.mint_size * test_config.total_iterations
         );
-        let transfer_size = 1290;
-        let full_transfer_size = (mint_size * 2 + transfer_size);
+        let full_transfer_size = (test_config.mint_size * 2 + test_config.transfer_size);
         const transfers_content = await readFile("./precomputed_transfers");
         const transfers_buffer = transfers_content.subarray(
-          transfers_offset,
-          full_transfer_size * total_iterations
+            test_config.transfers_offset,
+          full_transfer_size * test_config.total_iterations
         );
-        let reclaim_size = 968;
-        let full_reclaim_size = (mint_size * 2 + reclaim_size);
+        let full_reclaim_size = (test_config.mint_size * 2 + test_config.reclaim_size);
         const reclaims_content = await readFile("./precomputed_reclaims");
         const reclaims_buffer = reclaims_content.subarray(
-          reclaims_offset,
-          full_reclaim_size * total_iterations
+          test_config.reclaims_offset,
+          full_reclaim_size * test_config.total_iterations
         );
         let lastFinalized = false;
-        let finalizedCount = 0;
         let allSuccesses = 0;
         let txs_sent = 0;
         var startTime = performance.now()
         let totalTime = 0;
-        for(let i = start_iteration; i < start_iteration + tests_iterations; ++i){
-            await api.tx.mantaPay.toPrivate(mints_buffer.subarray(mint_size * i, mint_size * (i + 1))).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+        for(let i = test_config.start_iteration; i < test_config.start_iteration + test_config.tests_iterations; ++i){
+            await api.tx.mantaPay.toPrivate(mints_buffer.subarray(test_config.mint_size * i, test_config.mint_size * (i + 1)))
+            .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                 if (status.isInBlock) {
                     events.forEach(({ event: { data, method, section }, phase }) => {
                       if ('mantaPay.ToPrivate' == section + '.' + method ) {
@@ -79,9 +76,10 @@ describe('Node RPC Test', () => {
                     });
                 }
              });
-    
-            let transfers_start = i * (2 * mint_size + transfer_size);
-            await api.tx.mantaPay.toPrivate(transfers_buffer.subarray(transfers_start, transfers_start + mint_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+             
+            let transfers_start = i * (2 * test_config.mint_size + test_config.transfer_size);
+            await api.tx.mantaPay.toPrivate(transfers_buffer.subarray(transfers_start, transfers_start + test_config.mint_size))
+            .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                 if (status.isInBlock) {
                     events.forEach(({ event: { data, method, section }, phase }) => {
                       if ('mantaPay.ToPrivate' == section + '.' + method ) {
@@ -90,7 +88,8 @@ describe('Node RPC Test', () => {
                     });
                 }
              });
-             await api.tx.mantaPay.toPrivate(transfers_buffer.subarray(transfers_start + mint_size, transfers_start + 2 * mint_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+             await api.tx.mantaPay.toPrivate(transfers_buffer.subarray(transfers_start + test_config.mint_size, transfers_start + 2 * test_config.mint_size))
+             .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                 if (status.isInBlock) {
                     events.forEach(({ event: { data, method, section }, phase }) => {
                       if ('mantaPay.ToPrivate' == section + '.' + method ) {
@@ -99,7 +98,8 @@ describe('Node RPC Test', () => {
                     });
                 }
              });
-             await api.tx.mantaPay.privateTransfer(transfers_buffer.subarray(transfers_start + 2 * mint_size, transfers_start + 2 * mint_size + transfer_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+             await api.tx.mantaPay.privateTransfer(transfers_buffer.subarray(transfers_start + 2 * test_config.mint_size, transfers_start + 2 * test_config.mint_size + test_config.transfer_size))
+             .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                 if (status.isInBlock) {
                     events.forEach(({ event: { data, method, section }, phase }) => {
                       if ('mantaPay.PrivateTransfer' == section + '.' + method ) {
@@ -109,8 +109,9 @@ describe('Node RPC Test', () => {
                 }
              });
     
-            let reclaims_start = i * (2 * mint_size + reclaim_size);
-            await api.tx.mantaPay.toPrivate(reclaims_buffer.subarray(reclaims_start, reclaims_start + mint_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+            let reclaims_start = i * (2 * test_config.mint_size + test_config.reclaim_size);
+            await api.tx.mantaPay.toPrivate(reclaims_buffer.subarray(reclaims_start, reclaims_start + test_config.mint_size))
+            .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                 if (status.isInBlock) {
                     events.forEach(({ event: { data, method, section }, phase }) => {
                       if ('mantaPay.ToPrivate' == section + '.' + method ) {
@@ -119,7 +120,8 @@ describe('Node RPC Test', () => {
                     });
                 }
              });
-            await api.tx.mantaPay.toPrivate(reclaims_buffer.subarray(reclaims_start + mint_size, reclaims_start + 2 * mint_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+            await api.tx.mantaPay.toPrivate(reclaims_buffer.subarray(reclaims_start + test_config.mint_size, reclaims_start + 2 * test_config.mint_size))
+            .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                 if (status.isInBlock) {
                     events.forEach(({ event: { data, method, section }, phase }) => {
                       if ('mantaPay.ToPrivate' == section + '.' + method ) {
@@ -128,8 +130,9 @@ describe('Node RPC Test', () => {
                     });
                 }
              });
-            if (i == start_iteration + tests_iterations - 1) {
-                const unsub = await api.tx.mantaPay.toPublic(reclaims_buffer.subarray(reclaims_start + 2 * mint_size, reclaims_start + 2 * mint_size + reclaim_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+            if (i == test_config.start_iteration + test_config.tests_iterations - 1) {
+                const unsub = await api.tx.mantaPay.toPublic(reclaims_buffer.subarray(reclaims_start + 2 * test_config.mint_size, reclaims_start + 2 * test_config.mint_size + test_config.reclaim_size))
+                .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                     if (status.isInBlock) {
                         events.forEach(({ event: { data, method, section }, phase }) => {
                             let event = section + '.' + method;
@@ -138,7 +141,6 @@ describe('Node RPC Test', () => {
                             }
                           });
                     } else if (status.isFinalized) {
-                         finalizedCount++;
                          lastFinalized = true;
                          var endTime = performance.now()
                          totalTime = endTime - startTime;
@@ -147,7 +149,8 @@ describe('Node RPC Test', () => {
                     }
                  });
             } else {
-                await api.tx.mantaPay.toPublic(reclaims_buffer.subarray(reclaims_start + 2 * mint_size, reclaims_start + 2 * mint_size + reclaim_size)).signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
+                await api.tx.mantaPay.toPublic(reclaims_buffer.subarray(reclaims_start + 2 * test_config.mint_size, reclaims_start + 2 * test_config.mint_size + test_config.reclaim_size))
+                .signAndSend(sender, {nonce: -1}, ({ events = [], status }) => {
                     if (status.isInBlock) {
                         console.log('Included at block hash', status.asInBlock.toHex());
                         console.log('Events:');
@@ -173,7 +176,7 @@ describe('Node RPC Test', () => {
         for(let i = 0; i < test_config.max_wait_time_sec; i++){
             await delay(1000);
             if (lastFinalized) {
-                let tps = totalTime / (tests_iterations * 7);
+                let tps = totalTime / (test_config.tests_iterations * 7);
                 console.log("Tps is: ", tps);
                 assert(tps <= test_config.expected_tps);
                 break;
@@ -184,7 +187,7 @@ describe('Node RPC Test', () => {
             assert(false);
         }
 
-        if (allSuccesses != tests_iterations * 7) {
+        if (allSuccesses != test_config.tests_iterations * 7) {
             console.log("allSuccesses Count: ", allSuccesses);
             assert(false);
         }
