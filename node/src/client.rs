23e0f75358abd7f64693a76d7a2daa0646f7aee2
Adam Reif <Garandor@manta.network>
2022-12-22 15:59:16 -0600

    clippy fix
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- node/src/client.rs
+++ node/src/client.rs
@@ -17,7 +17,6 @@
 //! RuntimeApi for client
 
 use manta_primitives::types::{AccountId, Balance, Block, Index as Nonce};
-use session_key_primitives::NimbusId;
 use sp_runtime::traits::BlakeTwo256;
 
 /// RuntimeApiCommon + RuntimeApiNimbus: nimbus
