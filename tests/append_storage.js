b445689a78dca6f60caf5e7544b2b9eb02ee7fef
Georgi Zlatarev <georgi.zlatarev@manta.network>
2022-12-22 22:05:54 +0200

    Update gitattributes
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- tests/append_storage.js
+++ tests/append_storage.js
@@ -7,15 +7,18 @@ let prefixes = [
 ];
 
 async function main() {
-  let storage = JSON.parse(fs.readFileSync('./storage.json', 'utf8'));
-  let forkedSpec = JSON.parse(fs.readFileSync('./fork.json', 'utf8'));
+  let storagePath = './data/storage.json';
+  let forkPath = './data/fork.json';
+
+  let storage = JSON.parse(fs.readFileSync(storagePath, 'utf8'));
+  let forkedSpec = JSON.parse(fs.readFileSync(forkPath, 'utf8'));
 
   // Grab the items to be moved, then iterate through and insert into storage
   storage
   .filter((i) => prefixes.some((prefix) => i[0].startsWith(prefix)))
   .forEach(([key, value]) => (forkedSpec.genesis.raw.top[key] = value));
 
-  fs.writeFileSync('./fork.json', JSON.stringify(forkedSpec, null, 4));
+  fs.writeFileSync(forkPath, JSON.stringify(forkedSpec, null, 4));
 
   process.exit();
 }
