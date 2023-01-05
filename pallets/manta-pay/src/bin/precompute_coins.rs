1391a3f31ff5b770e9c2579c5beda730f74e4229
Adam Reif <garandor@manta.network>
2023-01-05 11:28:55 -0600

    dev-only: Happy new year and make release checklist a top-to-bottom todolist instead of separate sections (#962)

--- pallets/manta-pay/src/bin/precompute_coins.rs
+++ pallets/manta-pay/src/bin/precompute_coins.rs
@@ -1,4 +1,4 @@
-// Copyright 2020-2022 Manta Network.
+// Copyright 2020-2023 Manta Network.
 // This file is part of Manta.
 //
 // Manta is free software: you can redistribute it and/or modify
@@ -218,7 +218,7 @@ fn main() -> Result<()> {
     writeln!(
         target_file,
         indoc! {r"
-            // Copyright 2020-2022 Manta Network.
+            // Copyright 2020-2023 Manta Network.
             // This file is part of Manta.
             //
             // Manta is free software: you can redistribute it and/or modify
