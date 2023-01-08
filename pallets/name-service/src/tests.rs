31f7d89b35133e8eb60294478aadc16c64656c4b
zqhxuyuan <zqhxuyuan@gmail.com>
2023-01-08 22:57:40 +0800

    first draft
    Signed-off-by: zqhxuyuan <zqhxuyuan@gmail.com>

---
+++ pallets/name-service/src/tests.rs
@@ -0,0 +1,27 @@
+// Copyright 2020-2022 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+//
+//! Unit tests for the transaction pause pallet.
+
+#![cfg(test)]
+
+use super::*;
+use frame_support::{assert_noop, assert_ok};
+use frame_system::RawOrigin;
+use mock::{Event, *};
+
+#[test]
+fn pause_transaction_work() {}
