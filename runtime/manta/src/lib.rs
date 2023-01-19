5b726393b34c3caf1dbc57862c3646b516203252
Adam Reif <garandor@manta.network>
2023-01-19 08:45:21 -0600

    Release v4.0.1 (#994)

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -121,7 +121,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4001,
+    spec_version: 4010,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
