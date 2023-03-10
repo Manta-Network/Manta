f9a711cc23afe105828a04c24911e0852a4d513b
Adam Reif <Garandor@manta.network>
2023-03-09 21:34:51 -0600

    clean dupe
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -300,7 +300,6 @@ impl Contains<Call> for BaseFilter {
             | Call::XTokens(orml_xtokens::Call::transfer {..}
                 | orml_xtokens::Call::transfer_multicurrencies {..})
             | Call::TransactionPause(_)
-            | Call::Scheduler(_)
             | Call::Utility(_) => true,
 
             // DISALLOW anything else
