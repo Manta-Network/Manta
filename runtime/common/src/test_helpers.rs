84578806d247a9deeab0eb358524a6c8e124467a
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-03-24 17:28:51 +0200

    Fix cargo imports
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/common/src/test_helpers.rs
+++ runtime/common/src/test_helpers.rs
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Manta.  If not, see <http://www.gnu.org/licenses/>.
-#![cfg(test)]
 
 use sp_std::vec;
 use xcm::{
