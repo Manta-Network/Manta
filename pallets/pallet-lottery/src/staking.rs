4dd7f92971702350c886590cc1c1b495d5285707
Adam Reif <Garandor@manta.network>
2023-04-18 21:01:38 -0500

    wip: prefer staking to bottom, unstake from inactive collators
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- pallets/pallet-lottery/src/staking.rs
+++ pallets/pallet-lottery/src/staking.rs
@@ -14,10 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Manta.  If not, see <http://www.gnu.org/licenses/>.
 
+use codec::alloc::collections::BTreeSet;
 use pallet_parachain_staking::BalanceOf;
-
+use sp_std::{vec,vec::Vec};
 use super::*;
-use array_tool::vec::Intersect;
 use frame_support::ensure;
 use sp_runtime::traits::Saturating;
 use sp_runtime::traits::Zero;
@@ -32,10 +32,10 @@ impl<T: Config> Pallet<T> {
         let mut deposits: Vec<(T::AccountId, BalanceOf<T>)> = vec![];
 
         // first concern: If we fell out of the active set on one or more collators, we need to get back into it
-        let active_collators = pallet_parachain_staking::Pallet::<T>::selected_candidates();
-        let collators_we_are_staked_with: Vec<_> = StakedCollators::<T>::iter_keys().collect();
+        let active_collators = pallet_parachain_staking::Pallet::<T>::selected_candidates().into_iter().collect();
+        let collators_we_are_staked_with: BTreeSet<_> = StakedCollators::<T>::iter_keys().collect();
         let active_collators_we_are_staked_with =
-            collators_we_are_staked_with.intersect(active_collators);
+            collators_we_are_staked_with.intersection(&active_collators);
 
         let mut remaining_deposit = new_deposit;
         for collator in active_collators_we_are_staked_with {
@@ -45,7 +45,7 @@ impl<T: Config> Pallet<T> {
             if staked < info.lowest_top_delegation_amount {
                 let deposit =
                     remaining_deposit.saturating_sub(info.lowest_top_delegation_amount - staked);
-                deposits.push((collator, deposit));
+                deposits.push((collator.clone(), deposit));
                 remaining_deposit = remaining_deposit.saturating_sub(deposit);
                 if remaining_deposit.is_zero() {
                     break;
@@ -57,7 +57,8 @@ impl<T: Config> Pallet<T> {
             if !remaining_deposit.is_zero() {
                 // distribute remaining tokens evenly
                 let deposit_per_collator =
-                    sp_arithmetic::Percent::from_rational(1, deposits.len().into()).mul_ceil(remaining_deposit); // this overshoots the amount if there's a remainder
+                    sp_arithmetic::Percent::from_rational(1, deposits.len().into())
+                        .mul_ceil(remaining_deposit); // this overshoots the amount if there's a remainder
                 for deposit in &mut deposits {
                     let add = remaining_deposit.saturating_sub(deposit_per_collator);
                     deposit.1 += add;
@@ -67,18 +68,50 @@ impl<T: Config> Pallet<T> {
             return deposits;
         }
 
-        // // second concern: We want to maximize staking APY earned, so we want to balance the staking pools with our deposits while conserving gas
-        // let mut collator_balances: Vec<(T::AccountId,BalanceOf<T>)> = vec![];
-        // // We only consider active collators for deposits
-        // for collator in pallet_parachain_staking::Pallet::<T>::selected_candidates(){
-        //     let info = pallet_parachain_staking::Pallet::<T>::candidate_info(collator).expect("is active collator, therefor it has collator info. qed");
-        //     if ()
-        //     collator_balances.push((collator,info.total_counted));
-        // }
-        // let average_deposit = collator_balances.map(|x|x.1).mean();
-        // collator_balances.sort_unstable_by(|a,b| a.1.cmp(b.1));
-        // let eligible_for_deposit = collator_balances[..5]; // TODO: hardcoded ( to bound the gas cost )
-
+        // second concern: We want to maximize staking APY earned, so we want to balance the staking pools with our deposits while conserving gas
+        let mut collator_balances: Vec<(T::AccountId, BalanceOf<T>)> = vec![];
+        // We only consider active collators for deposits
+        for collator in &active_collators {
+            let info = pallet_parachain_staking::Pallet::<T>::candidate_info(collator)
+                .expect("is active collator, therefor it has collator info. qed");
+            let our_stake = StakedCollators::<T>::get(collator.clone()).clone();
+            if info.lowest_top_delegation_amount > our_stake {
+                collator_balances.push((
+                    collator.clone(),
+                    info.lowest_top_delegation_amount.saturating_sub(our_stake),
+                ));
+            }
+        }
+        // TODO let average_deposit = collator_balances.iter().map(|x| x.1).mean();
+        collator_balances.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+        let eligible_for_deposit = &collator_balances[..5]; // TODO: hardcoded ( to bound the gas cost of a single deposit )
+        // TODO
         deposits
+    }
+
+    fn calculate_withdrawal_distribution(withdrawal_amount: BalanceOf<T>) -> Vec<T::AccountId> {
+        if withdrawal_amount.is_zero() {
+            return vec![];
+        }
+        let mut withdrawals = vec![];
+        let mut remaining_balance = withdrawal_amount;
+        // first concern: If there are inactive collators we are staked with, prefer these
+        let active_collators: BTreeSet<_> = pallet_parachain_staking::Pallet::<T>::selected_candidates().into_iter().collect();
+        let collators_we_are_staked_with: BTreeSet<_> = StakedCollators::<T>::iter_keys().collect();
+        let inactive_collators_we_are_staked_with: BTreeSet<_> =
+            collators_we_are_staked_with.difference(&active_collators).cloned().collect();
+        for collator in inactive_collators_we_are_staked_with{
+            // unstake them
+            let balance = StakedCollators::<T>::get(&collator);
+            remaining_balance = remaining_balance.saturating_sub(balance);
+            withdrawals.push(collator.clone());
+            if remaining_balance.is_zero() {
+                return withdrawals;
+            }
+        }
+
+        // If we have balance to withdraw left over, we have to unstake some healthy collator. Choose the one with the smallest amount to cover the withdrawal amount
+        // TODO
+        vec![]
     }
 }
