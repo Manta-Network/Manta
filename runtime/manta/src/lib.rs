000a46f310df00bb45ac22ca42c0f3f4637c6edc
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-05-01 19:52:54 +0300

    4061
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -130,7 +130,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4060,
+    spec_version: 4061,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
