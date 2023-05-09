92a487d4a0446a65b732c776347a388a96f8ada1
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-05-09 16:49:38 +0300

    Update on_unbalanceds
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/manta/src/fee.rs
+++ runtime/manta/src/fee.rs
@@ -1,27 +1,27 @@
-// // Copyright 2020-2023 Manta Network.
-// // This file is part of Manta.
-// //
-// // Manta is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
-// //
-// // Manta is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// // GNU General Public License for more details.
-// //
-// // You should have received a copy of the GNU General Public License
-// // along with Manta.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright 2020-2023 Manta Network.
+// This file is part of Manta.
+//
+// Manta is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Manta is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Manta.  If not, see <http://www.gnu.org/licenses/>.
 
-// pub use sp_runtime::Perbill;
+pub use sp_runtime::Perbill;
 
-// pub const FEES_PERCENTAGE_TO_AUTHOR: u8 = 10;
-// pub const FEES_PERCENTAGE_TO_BURN: u8 = 45;
-// pub const FEES_PERCENTAGE_TO_TREASURY: u8 = 45;
+pub const FEES_PERCENTAGE_TO_AUTHOR: u8 = 10;
+pub const FEES_PERCENTAGE_TO_BURN: u8 = 45;
+pub const FEES_PERCENTAGE_TO_TREASURY: u8 = 45;
 
-// pub const TIPS_PERCENTAGE_TO_AUTHOR: u8 = 100;
-// pub const TIPS_PERCENTAGE_TO_TREASURY: u8 = 0;
+pub const TIPS_PERCENTAGE_TO_AUTHOR: u8 = 100;
+pub const TIPS_PERCENTAGE_TO_TREASURY: u8 = 0;
 
 // #[cfg(test)]
 // mod fee_split_tests {
