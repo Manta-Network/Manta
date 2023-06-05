a6075b5fb5c2ea82057d6fbcf674ecb14dcb5698
Charles Ferrell <charlie@manta.network>
2023-06-05 17:40:42 -0400

    update manta weights
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -844,7 +844,7 @@ pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, Si
 
 /// Types for runtime upgrading.
 /// Each type should implement trait `OnRuntimeUpgrade`.
-pub type OnRuntimeUpgradeHooks = migrations::assets_genesis::AssetsGenesis<Runtime>;
+pub type OnRuntimeUpgradeHooks = ();
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
     Runtime,
