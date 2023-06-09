e33423d4ae3d2a94006f30be68f78d712cc186ea
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-06-09 18:48:45 +0300

    Bump spec_versions and enable xcmp queue
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -134,7 +134,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("calamari"),
     impl_name: create_runtime_str!("calamari"),
     authoring_version: 2,
-    spec_version: 4100,
+    spec_version: 4101,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 13,
