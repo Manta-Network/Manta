38ebd5362c24cd698f457d69008fda1e6156f67e
Apokalip <simeon@manta.network>
2023-06-15 22:14:09 +0300

    spec
    Signed-off-by: Apokalip <simeon@manta.network>

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -136,7 +136,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4102,
+    spec_version: 4200,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
