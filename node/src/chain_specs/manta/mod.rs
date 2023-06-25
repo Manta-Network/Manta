bfc7031f2092b809e380d0ef55236f4de4ab1295
zqhxuyuan <zqhxuyuan@gmail.com>
2023-06-25 22:22:30 +0800

    manta local dev
    Signed-off-by: zqhxuyuan <zqhxuyuan@gmail.com>

--- node/src/chain_specs/manta/mod.rs
+++ node/src/chain_specs/manta/mod.rs
@@ -80,8 +80,8 @@ pub fn manta_testnet_config() -> MantaChainSpec {
     public_testnet_genesis::genesis_spec()
 }
 /// Returns the Manta development chainspec.
-pub fn manta_local_config() -> MantaChainSpec {
-    local_testnets_geneses::genesis_spec_local()
+pub fn manta_local_config(localdev: bool) -> MantaChainSpec {
+    local_testnets_geneses::genesis_spec_local(localdev)
 }
 /// Returns the Manta development chainspec.
 pub fn manta_development_config() -> MantaChainSpec {
