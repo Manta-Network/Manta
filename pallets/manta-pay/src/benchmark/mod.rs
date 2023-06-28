f0ed5957c3bc87b4a4a2631f26aceefb9607f069
Jamie <djptux@gmail.com>
2023-06-28 19:40:25 +0800

    Rebenchmark pallet-mantapay (#1150)
    * Rebenchmark pallet-mantapay
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Add node and yarn
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix yml
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix yml
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix ci
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix comments
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix ci
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * [no ci]Fix permission
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * [no ci]Fix path
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Remove precomputed coins and fix benchamrk ci
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix benchmark for asset manager
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix clippy
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Bump cache size
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Bump nightly rust
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix benchmark test
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Install protoc
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Fix compilation
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Bump sccache
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Bump sscache size
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Revert nightly rust
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Bump scache size and print disk usage
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Print disk usage
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Clean unsed deps
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    * Clean deps
    
    Signed-off-by: Dengjianping <djptux@gmail.com>
    
    ---------
    
    Signed-off-by: Dengjianping <djptux@gmail.com>

--- pallets/manta-pay/src/benchmark/mod.rs
+++ pallets/manta-pay/src/benchmark/mod.rs
@@ -14,12 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Manta.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{
-    benchmark::precomputed_coins::{
-        PRIVATE_TRANSFER, PRIVATE_TRANSFER_INPUT, TO_PRIVATE, TO_PUBLIC, TO_PUBLIC_INPUT,
-    },
-    Call, Config, Event, Pallet, StandardAssetId, TransferPost,
-};
+use crate::{Call, Config, Event, Pallet, StandardAssetId, TransferPost};
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_support::traits::Get;
 use frame_system::RawOrigin;
@@ -28,15 +23,25 @@ use manta_support::manta_pay::{
 };
 
 use manta_primitives::{
-    assets::{AssetConfig, AssetRegistry, FungibleLedger, TestingDefault},
+    assets::{AssetConfig, FungibleLedger},
     constants::TEST_DEFAULT_ASSET_ED,
     types::Balance,
 };
 use scale_codec::Decode;
 
-mod precomputed_coins;
-
 pub const INITIAL_VALUE: u128 = 1_000_000_000_000_000_000_000u128;
+
+const MINTS_OFFSET: usize = 2;
+const MINT_SIZE: usize = 553;
+
+const TRANSFERS_OFFSET: usize = 4;
+const TRANSFER_SIZE: usize = 1291;
+
+const RECLAIMS_OFFSET: usize = 4;
+const RECLAIM_SIZE: usize = 1001;
+
+// 14000 iterations of mint, mint, mint, transfer, mint, mint, reclaim
+const TOTAL_ITERATIONS: usize = 14000;
 
 /// Asserts that the last event that has occurred is the same as `event`.
 #[inline]
@@ -56,15 +61,6 @@ where
     T: Config,
     T::AccountId: From<AccountId> + Into<AccountId>,
 {
-    let metadata = <T::AssetConfig as AssetConfig<T>>::AssetRegistryMetadata::testing_default();
-    let storage_metadata: <T::AssetConfig as AssetConfig<T>>::StorageMetadata = metadata.into();
-    <T::AssetConfig as AssetConfig<T>>::AssetRegistry::create_asset(
-        id,
-        storage_metadata,
-        TEST_DEFAULT_ASSET_ED,
-        true,
-    )
-    .expect("Unable to create asset.");
     let pallet_account: T::AccountId = Pallet::<T>::account_id();
     <T::AssetConfig as AssetConfig<T>>::FungibleLedger::deposit_minting(
         id,
@@ -85,7 +81,10 @@ benchmarks! {
     to_private {
         let caller: T::AccountId = whitelisted_caller();
         let origin = T::RuntimeOrigin::from(RawOrigin::Signed(caller.clone()));
-        let mint_post = TransferPost::decode(&mut &*TO_PRIVATE).unwrap();
+        let mint_coins = core::include_bytes!("../../../../tests/data/precomputed_mints");
+        let mints_start = MINTS_OFFSET + TOTAL_ITERATIONS * MINT_SIZE;
+        let to_private_coin = &mint_coins[mints_start..mints_start + MINT_SIZE];
+        let mint_post = TransferPost::decode(&mut &*to_private_coin).unwrap();
         let asset = mint_post.source(0).unwrap();
         init_asset::<T>(&caller, id_from_field(asset.id).unwrap(), asset_value_decode(asset.value));
     }: to_private (
@@ -100,13 +99,25 @@ benchmarks! {
         let caller: T::AccountId = whitelisted_caller();
         let origin = T::RuntimeOrigin::from(RawOrigin::Signed(caller.clone()));
         init_asset::<T>(&caller, <T::AssetConfig as AssetConfig<T>>::StartNonNativeAssetId::get(), INITIAL_VALUE);
-        for coin in TO_PUBLIC_INPUT {
+        let reclaim_coins = core::include_bytes!("../../../../tests/data/precomputed_reclaims");
+
+        let reclaim_start = RECLAIMS_OFFSET + TOTAL_ITERATIONS * (2 * MINT_SIZE + RECLAIM_SIZE);
+        let to_public_input = {
+            let mint_post1 = &reclaim_coins[reclaim_start..reclaim_start + MINT_SIZE];
+            let mint_post2 = &reclaim_coins[reclaim_start + MINT_SIZE..reclaim_start + 2 * MINT_SIZE];
+
+            [mint_post1, mint_post2]
+        };
+
+        for coin in to_public_input {
             Pallet::<T>::to_private(
                 origin.clone(),
-                TransferPost::decode(&mut &**coin).unwrap()
+                TransferPost::decode(&mut &*coin).unwrap()
             ).unwrap();
         }
-        let reclaim_post = TransferPost::decode(&mut &*TO_PUBLIC).unwrap();
+
+        let to_public_post = &reclaim_coins[reclaim_start + 2 * MINT_SIZE..reclaim_start + 2 * MINT_SIZE + RECLAIM_SIZE];
+        let reclaim_post = TransferPost::decode(&mut &*to_public_post).unwrap();
         let asset = reclaim_post.sink(0).unwrap();
     }: to_public (
         RawOrigin::Signed(caller.clone()),
@@ -120,13 +131,23 @@ benchmarks! {
         let caller: T::AccountId = whitelisted_caller();
         let origin = T::RuntimeOrigin::from(RawOrigin::Signed(caller.clone()));
         init_asset::<T>(&caller, <T::AssetConfig as AssetConfig<T>>::StartNonNativeAssetId::get(), INITIAL_VALUE);
-        for coin in PRIVATE_TRANSFER_INPUT {
+        let private_transfer_coins = core::include_bytes!("../../../../tests/data/precomputed_transfers");
+        let transfer_start = TRANSFERS_OFFSET + TOTAL_ITERATIONS * (2 * MINT_SIZE + TRANSFER_SIZE);
+        let private_transfer_input = {
+            let mint_post1 = &private_transfer_coins[transfer_start..transfer_start + MINT_SIZE];
+            let mint_post2 = &private_transfer_coins[transfer_start + MINT_SIZE..transfer_start + 2 * MINT_SIZE];
+
+            [mint_post1, mint_post2]
+        };
+
+        for coin in private_transfer_input {
             Pallet::<T>::to_private(
                 origin.clone(),
-                TransferPost::decode(&mut &**coin).unwrap(),
+                TransferPost::decode(&mut &*coin).unwrap(),
             ).unwrap();
         }
-        let private_transfer_post = TransferPost::decode(&mut &*PRIVATE_TRANSFER).unwrap();
+        let private_transfer_post = &private_transfer_coins[transfer_start + 2 * MINT_SIZE..transfer_start + 2 * MINT_SIZE + TRANSFER_SIZE];
+        let private_transfer_post = TransferPost::decode(&mut &*private_transfer_post).unwrap();
     }: private_transfer (
         RawOrigin::Signed(caller.clone()),
         private_transfer_post
