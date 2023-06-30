04bf4ec03cbe02a3bf64fe458b3d5833248398ec
Adam Reif <Garandor@manta.network>
2023-06-30 15:48:27 -0700

    cleanup
    Signed-off-by: Adam Reif <Garandor@manta.network>

--- pallets/pallet-lottery/src/lib.rs
+++ pallets/pallet-lottery/src/lib.rs
@@ -162,7 +162,6 @@ pub mod pallet {
 
     #[pallet::pallet]
     #[pallet::storage_version(STORAGE_VERSION)]
-    // #[pallet::generate_store(pub(super) trait Store)]
     #[pallet::without_storage_info]
     pub struct Pallet<T>(PhantomData<T>);
 
