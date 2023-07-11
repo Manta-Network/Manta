2eb6a3175a961bac2009fecfb334b1a9b787a8e9
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-07-11 15:29:45 +0300

    benchmark only znelink
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- scripts/benchmarking/run_all_benchmarks.sh
+++ scripts/benchmarking/run_all_benchmarks.sh
@@ -117,7 +117,7 @@ rm -f $MACHINE_OUTPUT
 # Benchmark each frame pallet.
 for PALLET in "${PALLETS[@]}"; do
   # If `-p` is used, skip benchmarks until the start pallet.
-  if [ ! -z "$start_pallet" ] && [ "$start_pallet" != "$PALLET" ]
+  if [ "zenlink_protocol" != "$PALLET" ]
   then
     echo "[+] Skipping ${PALLET}..."
     continue
