5c37a5dbe25d377c86050150d2804a3215b54639
Georgi Zlatarev <45011053+ghzlatarev@users.noreply.github.com>
2023-07-12 19:45:15 +0300

    Allow xcm-transacts from all chains (#1055)
    * Allow xcm-trasnacts from all chains
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * Add xcm-transact to Dolphin and Manta
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * Comments
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * Add test
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * Fix clippy
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * Add missing AllowTopLevelPaidExecutionFrom
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    ---------
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/integration-tests/src/xcm_mock/parachain.rs
+++ runtime/integration-tests/src/xcm_mock/parachain.rs
@@ -44,7 +44,10 @@ use manta_primitives::{
     },
     constants::{ASSET_MANAGER_PALLET_ID, CALAMARI_DECIMAL, WEIGHT_PER_SECOND},
     types::{BlockNumber, CalamariAssetId, Header},
-    xcm::{FirstAssetTrader, IsNativeConcrete, MultiAssetAdapter, MultiNativeAsset},
+    xcm::{
+        AllowTopLevelPaidExecutionDescendOriginFirst, FirstAssetTrader, IsNativeConcrete,
+        MultiAssetAdapter, MultiNativeAsset,
+    },
 };
 use pallet_xcm::XcmPassthrough;
 use polkadot_core_primitives::BlockNumber as RelayBlockNumber;
@@ -53,7 +56,7 @@ use polkadot_parachain::primitives::{
 };
 use xcm::{latest::prelude::*, Version as XcmVersion, VersionedMultiLocation, VersionedXcm};
 use xcm_builder::{
-    AccountId32Aliases, AllowKnownQueryResponses, AllowSubscriptionsFrom,
+    Account32Hash, AccountId32Aliases, AllowKnownQueryResponses, AllowSubscriptionsFrom,
     AllowTopLevelPaidExecutionFrom, AllowUnpaidExecutionFrom, ConvertedConcreteAssetId,
     EnsureXcmOrigin, FixedRateOfFungible, LocationInverter, ParentIsPreset,
     SiblingParachainAsNative, SiblingParachainConvertsVia, SignedAccountId32AsNative,
@@ -176,6 +179,8 @@ pub type LocationToAccountId = (
     // Sibling parachain origins convert to AccountId via the `ParaId::into`.
     SiblingParachainConvertsVia<Sibling, AccountId>,
     AccountId32Aliases<RelayNetwork, AccountId>,
+    // Converts multilocation into a 32 byte hash for local `AccountId`s
+    Account32Hash<RelayNetwork, AccountId>,
 );
 
 /// This is the type to convert an (incoming) XCM origin into a local `Origin` instance,
@@ -241,6 +246,9 @@ match_types! {
 pub type Barrier = (
     // Allows local origin messages which call weight_credit >= weight_limit.
     TakeWeightCredit,
+    // Allows execution of Transact XCM instruction from configurable set of origins
+    // as long as the message is in the format DescendOrigin + WithdrawAsset + BuyExecution
+    AllowTopLevelPaidExecutionDescendOriginFirst<Everything>,
     // Allows non-local origin messages, for example from from the xcmp queue,
     // which have the ability to deposit assets and pay for their own execution.
     AllowTopLevelPaidExecutionFrom<Everything>,
