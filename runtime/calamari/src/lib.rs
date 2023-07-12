0657ef7f8275ff42419483d67521a0c766cae457
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-07-12 17:37:28 +0300

    bump spec version
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("calamari"),
     impl_name: create_runtime_str!("calamari"),
     authoring_version: 2,
-    spec_version: 4300,
+    spec_version: 4301,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 14,
