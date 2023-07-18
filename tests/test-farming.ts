74f11b62ba91a8c56c5a92c140db810698570223
zqhxuyuan <zqhxuyuan@gmail.com>
2023-07-18 19:06:42 +0800

    block
    Signed-off-by: zqhxuyuan <zqhxuyuan@gmail.com>

--- tests/test-farming.ts
+++ tests/test-farming.ts
@@ -154,9 +154,10 @@ describe('Node RPC Test', () => {
         // 10(decimal:18)
         callData = api.tx.assetManager.registerAsset(local_asset(parachainId, "MANDEX"), metadata3);
         await execute_via_governance(api, alice, callData, referendumIndexObject);
-        state = await api.query.assetManager.assetIdMetadata(10);
+        let mandexId = 10;
+        state = await api.query.assetManager.assetIdMetadata(mandexId);
         while(state.isNone) {
-            state = await api.query.assetManager.assetIdMetadata(10);
+            state = await api.query.assetManager.assetIdMetadata(mandexId);
             await timer(3000);
         }
         console.log(new Date() + " Register Asset10:" + JSON.stringify(state));
@@ -164,9 +165,10 @@ describe('Node RPC Test', () => {
         // register lp asset 11(decimal:12)
         callData = api.tx.assetManager.registerLpAsset(8, 9, lp_metadata);
         await execute_via_governance(api, alice, callData, referendumIndexObject);
-        state = await api.query.assetManager.assetIdMetadata(11);
+        let lpAssetId = 11;
+        state = await api.query.assetManager.assetIdMetadata(lpAssetId);
         while(state.isNone) {
-            state = await api.query.assetManager.assetIdMetadata(11);
+            state = await api.query.assetManager.assetIdMetadata(lpAssetId);
             await timer(3000);
         }
         console.log(new Date() + " Register LP Asset11:" + JSON.stringify(state));
@@ -183,7 +185,7 @@ describe('Node RPC Test', () => {
         await execute_via_governance(api, alice, callData, referendumIndexObject);
         callData = api.tx.assetManager.mintAsset(9, alice.address, new BN("200000000000000000"));
         await execute_via_governance(api, alice, callData, referendumIndexObject);
-        callData = api.tx.assetManager.mintAsset(10, alice.address, new BN("1000000000000000000000000"));
+        callData = api.tx.assetManager.mintAsset(mandexId, alice.address, new BN("1000000000000000000000000"));
         await execute_via_governance(api, alice, callData, referendumIndexObject);
 
         console.log(new Date() + " Mint Asset block:" + Number(await api.query.system.number()));
@@ -201,23 +203,23 @@ describe('Node RPC Test', () => {
         console.log(new Date() + " Add Liquidity block:" + Number(await api.query.system.number()));
 
         // create farming pool: stake 11(LP), reward 10(MANDEX)
-        callData = api.tx.farming.createFarmingPool([[11, 1000000000]], [[10, new BN("1000000000000000000")]], null, 10000000000000, 1, 0, 0, 2);
+        callData = api.tx.farming.createFarmingPool([[lpAssetId, 1000000000]], [[mandexId, new BN("1000000000000000000")]], null, 10000000000000, 1, 0, 0, 2);
         await execute_via_governance(api, alice, callData, referendumIndexObject);
 
         await timer(13000);
 
+        state = await api.query.farming.poolInfos(0);
+        console.log(new Date() + " Query farming pool0:" + JSON.stringify(state));
+
         // charge reward token to farming pool account
-        callData = api.tx.farming.charge(0, [[10, new BN("1000000000000000000000")]]);
+        callData = api.tx.farming.charge(0, [[mandexId, new BN("1000000000000000000000")]]);
         await execute_transaction(api, alice, callData, false);
+
+        await timer(13000);
 
         // mock new block
         callData = api.tx.system.remark("0x00");
         await execute_transaction(api, alice, callData, false);
-
-        callData = api.tx.system.remark("0x00");
-        await execute_transaction(api, alice, callData, false);
-
-        await timer(13000);
 
         state = await api.query.zenlinkProtocol.pairStatuses([[parachainId,2,8], [parachainId,2,9]]);
         let json = JSON.parse(JSON.stringify(state));
@@ -226,7 +228,7 @@ describe('Node RPC Test', () => {
 
         state = await api.query.farming.poolInfos(0);
         json = JSON.parse(JSON.stringify(state));
-        console.log(new Date() + " Query farming pool0:" + JSON.stringify(state));
+        console.log(new Date() + " Query farming pool1:" + JSON.stringify(state));
         expect(json.state).to.deep.equal("Charged");
 
         // user deposit lp token to farming pool
@@ -236,14 +238,12 @@ describe('Node RPC Test', () => {
         // mock new block
         callData = api.tx.system.remark("0x00");
         await execute_transaction(api, alice, callData, false);
-        callData = api.tx.system.remark("0x00");
-        await execute_transaction(api, alice, callData, false);
 
         await timer(13000);
 
         state = await api.query.farming.poolInfos(0);
         json = JSON.parse(JSON.stringify(state));
-        console.log(new Date() + " Query farming pool1:" + JSON.stringify(state));
+        console.log(new Date() + " Query farming pool2:" + JSON.stringify(state));
         expect(json.state).to.deep.equal("Ongoing");
 
         state = await api.query.farming.sharesAndWithdrawnRewards(0, alice.address);
