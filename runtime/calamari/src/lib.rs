548ab2a346976495d183c3c15dcb9f28d19cdca3
Charles Ferrell <charlie@manta.network>
2023-07-20 18:21:37 -0400

    bump spec version
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -143,10 +143,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("calamari"),
     impl_name: create_runtime_str!("calamari"),
     authoring_version: 2,
-    spec_version: 4310,
+    spec_version: 4400,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 14,
+    transaction_version: 15,
     state_version: 0,
 };
 
