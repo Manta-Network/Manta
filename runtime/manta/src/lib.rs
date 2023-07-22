c869bc9a68a819fe4004d98245951c25aa9eb752
ferrell-code <charlie@manta.network>
2023-07-22 11:54:19 -0400

    Release v4.4.0 (#1229)
    * bump versions
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * bump spec version
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * update changelog
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * Unfilter transfer_multiasset_with_fee
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * cargo fmt
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * filter lottery
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * do only lottery pallet
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * filter pallet_lottery
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * update manta weights
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * only lottery
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * Only lottery
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * Revert scripts/
    
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    
    * calamari weights
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    ---------
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>
    Co-authored-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -145,10 +145,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4310,
+    spec_version: 4400,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 3,
+    transaction_version: 4,
     state_version: 1,
 };
 
@@ -269,7 +269,9 @@ impl Contains<RuntimeCall> for MantaFilter {
                 | pallet_parachain_staking::Call::schedule_delegator_bond_less{..}
                 | pallet_parachain_staking::Call::execute_delegation_request{..}
                 | pallet_parachain_staking::Call::cancel_delegation_request{..})
-            | RuntimeCall::XTokens(orml_xtokens::Call::transfer {..} | orml_xtokens::Call::transfer_multiassets {..})
+            | RuntimeCall::XTokens(orml_xtokens::Call::transfer {..}
+                | orml_xtokens::Call::transfer_multiassets {..}
+                | orml_xtokens::Call::transfer_multiasset_with_fee {..})
             | RuntimeCall::Balances(_)
             | RuntimeCall::Preimage(_)
             | RuntimeCall::MantaPay(_)
