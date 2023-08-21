76967016b6055472498bce2146d3260886955e2e
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-08-21 10:40:24 +0300

    Revert ignore
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/calamari/src/diff_tx_fees.rs
+++ runtime/calamari/src/diff_tx_fees.rs
@@ -64,7 +64,6 @@ fn get_call_details(call: &crate::RuntimeCall) -> (DispatchInfo, u32) {
 }
 
 #[test]
-#[ignore] // TODO: remove and fix
 fn diff_tx_fees() {
     const CURRENT_PATH: &str = env!("CARGO_MANIFEST_DIR");
     let mut latest_version = String::new();
