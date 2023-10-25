d432d80ccfc014ce978297f828ced47ad0ea3bf7
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-10-25 05:07:42 +0300

    Bump spec version
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -145,7 +145,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4401,
+    spec_version: 4410,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 5,
