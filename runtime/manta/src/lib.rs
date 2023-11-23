d962677186d2f9c4e8f2ac956e96d0b592943045
ferrell-code <charlie@manta.network>
2023-11-22 22:27:49 -0500

    Release v4.5.0 (#1264)
    * bump version and spec
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * update changelog
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * update ci polkadot version
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    ---------
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -145,10 +145,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4402,
+    spec_version: 4500,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 5,
+    transaction_version: 6,
     state_version: 1,
 };
 
