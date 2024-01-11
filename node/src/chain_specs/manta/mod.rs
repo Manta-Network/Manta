02dc4fe650bec13a202bc823d5e08f90289e345c
Charles Ferrell <charlie@manta.network>
2024-01-11 18:02:32 -0500

    clippy
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- node/src/chain_specs/manta/mod.rs
+++ node/src/chain_specs/manta/mod.rs
@@ -91,8 +91,6 @@ pub fn manta_development_config() -> MantaChainSpec {
 
 // common helper to create the above configs
 fn manta_devnet_genesis(genesis_collators: Vec<Collator>) -> GenesisConfig {
-    let root_key = genesis_collators.first().unwrap().acc.clone();
-
     const INITIAL_COLLATOR_BALANCE: Balance = 1_000_000_000 * MANTA;
     let endowments = genesis_collators
         .iter()
