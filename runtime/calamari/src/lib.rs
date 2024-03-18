3d2cd879d1ca60d08df1335063c86f4c6a88e26d
Dengjianping <djptux@gmail.com>
2024-03-18 16:12:39 +0800

    Bump versions
    Signed-off-by: Dengjianping <djptux@gmail.com>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -144,7 +144,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("calamari"),
     impl_name: create_runtime_str!("calamari"),
     authoring_version: 2,
-    spec_version: 4611,
+    spec_version: 4620,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 18,
