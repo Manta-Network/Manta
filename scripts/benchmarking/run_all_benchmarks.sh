1ca80725d848d78214730895e01d582d54266162
Jamie <djptux@gmail.com>
2024-05-17 15:14:05 +0800

    Fix benchmarkng
    Signed-off-by: Jamie <djptux@gmail.com>

--- scripts/benchmarking/run_all_benchmarks.sh
+++ scripts/benchmarking/run_all_benchmarks.sh
@@ -79,7 +79,6 @@ MANTA=./target/production/manta
 # Will fix it in next release
 # currently, both pallets have problems on benchmarking
 EXCLUDED_PALLETS=(
-  "zenlink_protocol"
 )
 
 # Load all pallet names in an array.
