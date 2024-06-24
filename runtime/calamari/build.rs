201d8b4539933d733075ecfeb8f00bc5247e4590
Charles Ferrell <charlie@manta.network>
2024-06-24 11:55:50 -0400

    use KMA for token
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- runtime/calamari/build.rs
+++ runtime/calamari/build.rs
@@ -17,7 +17,7 @@
 #[cfg(all(feature = "std", feature = "metadata-hash"))]
 fn main() {
     substrate_wasm_builder::WasmBuilder::init_with_defaults()
-        .enable_metadata_hash("CALAMARI", 12)
+        .enable_metadata_hash("KMA", 12)
         .build();
 }
 
